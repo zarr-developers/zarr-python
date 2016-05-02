3bd65775c893108b2138130ce3ba1b48daccc6eb
Alistair Miles <alimanfoo@googlemail.com>
2016-05-03 00:32:32 +0100

    unmock

--- docs/conf.py
+++ docs/conf.py
@@ -22,16 +22,17 @@ import os
 sys.path.insert(0, os.path.abspath('..'))
 
 
-from mock import Mock as MagicMock
+# from mock import Mock as MagicMock
+#
+#
+# class Mock(MagicMock):
+#     @classmethod
+#     def __getattr__(cls, name):
+#             return Mock()
+#
+# MOCK_MODULES = ['zarr.blosc']
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
-
-MOCK_MODULES = ['zarr.blosc']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
