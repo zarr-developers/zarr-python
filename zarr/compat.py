17cb7511acb737d3d8b5edbd07a0d90961981eb5
Alistair Miles <alimanfoo@googlemail.com>
2017-10-24 23:01:46 +0100

    use numbers.Integral

--- zarr/compat.py
+++ zarr/compat.py
@@ -4,26 +4,17 @@ from __future__ import absolute_import, print_function, division
 import sys
 
 
-import numpy as np
-
-
 PY2 = sys.version_info[0] == 2
-
-
-numpy_integer_types = np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, \
-                      np.uint64
 
 
 if PY2:  # pragma: no cover
 
     text_type = unicode
     binary_type = str
-    integer_types = (int, long) + numpy_integer_types
     reduce = reduce
 
 else:
 
     text_type = str
     binary_type = bytes
-    integer_types = (int,) + numpy_integer_types
     from functools import reduce
