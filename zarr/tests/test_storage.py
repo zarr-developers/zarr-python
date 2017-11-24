ae1358302c268c10e809aba0d368950db548c5ed
Alistair Miles <alimanfoo@googlemail.com>
2017-11-24 01:39:53 +0000

    remove commented tests

--- zarr/tests/test_storage.py
+++ zarr/tests/test_storage.py
@@ -309,10 +309,6 @@ class StoreTests(object):
         eq(default_compressor.get_config(), meta['compressor'])
         assert_is_none(meta['fill_value'])
 
-        # check attributes
-        # assert attrs_key in store
-        # eq(dict(), json.loads(text_type(store[attrs_key], 'ascii')))
-
     def test_init_array_overwrite(self):
         # setup
         store = self.create_store()
@@ -359,11 +355,6 @@ class StoreTests(object):
         eq(np.dtype(None), meta['dtype'])
         eq(default_compressor.get_config(), meta['compressor'])
         assert_is_none(meta['fill_value'])
-
-        # check attributes
-        # key = path + '/' + attrs_key
-        # assert key in store
-        # eq(dict(), json.loads(text_type(store[key], 'ascii')))
 
     def test_init_array_overwrite_path(self):
         # setup
@@ -475,10 +466,6 @@ class StoreTests(object):
         assert group_meta_key in store
         meta = decode_group_metadata(store[group_meta_key])
         eq(ZARR_FORMAT, meta['zarr_format'])
-
-        # check attributes
-        # assert attrs_key in store
-        # eq(dict(), json.loads(text_type(store[attrs_key], 'ascii')))
 
     def test_init_group_overwrite(self):
         # setup
