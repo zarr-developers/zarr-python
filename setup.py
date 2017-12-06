116d16828609f61df0b50a431494d98aaef04eb6
Alistair Miles <alimanfoo@googlemail.com>
2017-12-06 09:15:49 +0000

    bump numcodecs

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'asciitree',
         'numpy>=1.7',
         'fasteners',
-        'numcodecs>=0.4.1',
+        'numcodecs>=0.5.0',
     ],
     package_dir={'': '.'},
     packages=['zarr', 'zarr.tests'],
