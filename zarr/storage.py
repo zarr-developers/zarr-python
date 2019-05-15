01025bb3195ef96214dca265191524c737ec583b
potter420 <thanhmt0612@gmail.com>
2019-05-15 10:07:34 +0700

    Travis CI Coverage
    Changes to comply with travis build

--- zarr/storage.py
+++ zarr/storage.py
@@ -2185,12 +2185,12 @@ class SQLiteStore(MutableMapping):
         path = normalize_storage_path(path)
         sep = '_' if path == '' else '/'
         keys = self.cursor.execute(
-            f'''
+            '''
             SELECT DISTINCT SUBSTR(m, 0, INSTR(m, "/")) AS l FROM (
                 SELECT LTRIM(SUBSTR(k, LENGTH(?) + 1), "/") || "/" AS m
                 FROM zarr WHERE k LIKE (? || "{sep}%")
             ) ORDER BY l ASC
-            ''',
+            '''.format(sep=sep),
             (path, path)
         )
         keys = list(map(operator.itemgetter(0), keys))
