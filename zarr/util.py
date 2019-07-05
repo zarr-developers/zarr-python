5f5663f94dd577610fa1ec05b29f9de4e4ed18eb
John Kirkham <jakirkham@gmail.com>
2019-07-05 11:44:31 -0400

    Disable all nodes to disallow selection
    As the point of our tree display is not to select nodes, disable this
    feature. Hopefully works around a bug where icons disappear in nbviewer
    upon selection.

--- zarr/util.py
+++ zarr/util.py
@@ -438,6 +438,7 @@ def tree_widget_sublist(node, root=False, expand=False):
         result.opened = False
     result.name = node.get_text()
     result.nodes = [tree_widget_sublist(c, expand) for c in node.get_children()]
+    result.disabled = True
 
     return result
 
