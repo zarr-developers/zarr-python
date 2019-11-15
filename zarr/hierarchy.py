c43ec941fdf1be4dda51252dff34d3e4199746a3
John Kirkham <jakirkham@gmail.com>
2019-11-15 00:32:28 -0500

    Include `_meta_array` when pickling `Group`
    To make sure that libraries that rely on pickling objects (like Dask or
    other parallel libraries) respect the `_meta_array` attribute, pass it
    along as part of the object's state when `__getstate__` is called. Since
    `__setstate__` just calls the constructor with all arguments, we can be
    sure that `_meta_array` will then be passed back into the new `Group`.
    Though this all assumes that the `_meta_array` is something that can be
    pickled.

--- zarr/hierarchy.py
+++ zarr/hierarchy.py
@@ -270,7 +270,7 @@ class Group(MutableMapping):
 
     def __getstate__(self):
         return (self._store, self._path, self._read_only, self._chunk_store,
-                self.attrs.cache, self._synchronizer)
+                self.attrs.cache, self._synchronizer, self._meta_array)
 
     def __setstate__(self, state):
         self.__init__(*state)
