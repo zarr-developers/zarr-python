4e27120acc859de8556ab5fa0e9f4570158d5f71
Zain Patel <zain.patel@quantumblack.com>
2020-04-14 20:35:42 +0100

    Update error message with conda

--- zarr/tests/test_util.py
+++ zarr/tests/test_util.py
@@ -169,9 +169,10 @@ def test_tree_get_icon():
 @mock.patch.dict("sys.modules", {"ipytree": None})
 def test_tree_widget_missing_ipytree():
     pattern = (
-        "Run `pip install zarr[jupyter]` to get the required ipytree "
-        "dependency for displaying the tree widget. If using jupyterlab, "
-        "you also need to run `jupyter labextension install ipytree`"
+        "Run `pip install zarr[jupyter]` or `conda install ipytree`"
+        "to get the required ipytree dependency for displaying the tree "
+        "widget. If using jupyterlab, you also need to run "
+        "`jupyter labextension install ipytree`"
         )
     with pytest.raises(ImportError, match=re.escape(pattern)):
         tree_widget(None, None, None)
