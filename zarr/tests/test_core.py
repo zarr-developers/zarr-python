9003e4ee0382528ed8a614796f334c867b776264
Andrew Fulton <andrewfulton9@gmail.com>
2020-11-04 17:49:05 -0700

    dont need to explicitly close arrays anymore

--- zarr/tests/test_core.py
+++ zarr/tests/test_core.py
@@ -2389,31 +2389,22 @@ class TestArrayWithFSStore(TestArray):
         # Check basic 1-D array
         z = self.create_array(shape=(1050,), chunks=100, dtype='<i4')
         assert 'f710da18d45d38d4aaf2afd7fb822fdd73d02957' == z.hexdigest()
-        if hasattr(z.store, 'close'):
-            z.store.close()
 
         # Check basic 1-D array with different type
         z = self.create_array(shape=(1050,), chunks=100, dtype='<f4')
         assert '1437428e69754b1e1a38bd7fc9e43669577620db' == z.hexdigest()
-        if hasattr(z.store, 'close'):
-            z.store.close()
 
         # Check basic 2-D array
         z = self.create_array(shape=(20, 35,), chunks=10, dtype='<i4')
         assert '6c530b6b9d73e108cc5ee7b6be3d552cc994bdbe' == z.hexdigest()
-        if hasattr(z.store, 'close'):
-            z.store.close()
 
         # Check basic 1-D array with some data
         z = self.create_array(shape=(1050,), chunks=100, dtype='<i4')
         z[200:400] = np.arange(200, 400, dtype='i4')
         assert '4c0a76fb1222498e09dcd92f7f9221d6cea8b40e' == z.hexdigest()
-        if hasattr(z.store, 'close'):
-            z.store.close()
 
         # Check basic 1-D array with attributes
         z = self.create_array(shape=(1050,), chunks=100, dtype='<i4')
         z.attrs['foo'] = 'bar'
         assert '05b0663ffe1785f38d3a459dec17e57a18f254af' == z.hexdigest()
-        if hasattr(z.store, 'close'):
-            z.store.close()
+
