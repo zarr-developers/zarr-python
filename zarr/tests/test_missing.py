0a33f442d386f3d04d4123ea4e4f716f4bcb5477
Willi Rath <wrath@geomar.de>
2021-04-21 20:28:06 +0200

    Add test for zero-dim array

--- zarr/tests/test_missing.py
+++ zarr/tests/test_missing.py
@@ -3,7 +3,23 @@ from zarr.creation import array
 
 
 class TestArrayMissingKeys(unittest.TestCase):
-    def test_raises_on_missing_key(self):
+    def test_raises_on_missing_key_zd(self):
+        a = array(1, chunks=1)
+
+        # pop first chunk
+        a.chunk_store.pop("0")
+
+        # read from missing chunk and make sure fill-value is returned
+        assert a.fill_value == a[()]
+
+        # configure raise on missing chunk
+        a.set_options(fill_missing_chunk=False)
+
+        # reading missing chunk should raise
+        with self.assertRaises(KeyError):
+            a[()]
+
+    def test_raises_on_missing_key_1d(self):
         a = array(range(4), chunks=2)
 
         # pop first chunk
