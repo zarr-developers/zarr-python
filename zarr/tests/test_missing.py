6690a6d93300069825bcbf65df9c55bb6a4be74f
Willi Rath <wrath@geomar.de>
2021-04-21 11:51:17 +0200

    Fix test logic

--- zarr/tests/test_missing.py
+++ zarr/tests/test_missing.py
@@ -6,9 +6,6 @@ class TestArrayMissingKeys(unittest.TestCase):
     def test_raises_on_missing_key(self):
         a = array(range(4), chunks=2)
 
-        # configure raise on missing chunk
-        a.set_options(fill_missing_chunk=False)
-
         # pop first chunk
         a.chunk_store.pop("0")
 
@@ -16,9 +13,12 @@ class TestArrayMissingKeys(unittest.TestCase):
         assert a.fill_value == a[0]
         assert a.fill_value == a[1]
 
-        # read from avaible chunk w/o error    
-        assert 2 = a[2]
-        assert 3 = a[3]
+        # read from avaible chunk w/o error
+        assert 2 == a[2]
+        assert 3 == a[3]
+
+        # configure raise on missing chunk
+        a.set_options(fill_missing_chunk=False)
 
         # reading missing chunk should raise
         with self.assertRaises(KeyError):
