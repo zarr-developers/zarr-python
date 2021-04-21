806e0be850c597449ced97b5121f6f095a09ba81
Willi Rath <wrath@geomar.de>
2021-04-21 10:46:08 +0200

    Also test for slicing

--- zarr/tests/test_missing.py
+++ zarr/tests/test_missing.py
@@ -4,7 +4,7 @@ from zarr.creation import array
 
 class TestArrayMissingKeys(unittest.TestCase):
     def test_raises_on_missing_key(self):
-        a = array(range(2), chunks=1)
+        a = array(range(4), chunks=2)
 
         # configure raise on missing chunk
         a.set_options(fill_missing_chunk=False)
@@ -12,9 +12,13 @@ class TestArrayMissingKeys(unittest.TestCase):
         # pop first chunk
         a.chunk_store.pop("0")
 
-        # read avaible chunk w/o error
+        # read from avaible chunk w/o error
         b = a[-1]
+        c = a[-2:]
 
         # reading missing chunk should raise
         with self.assertRaises(KeyError):
             b = a[0]
+
+        with self.assertRaises(KeyError):
+            c = a[:2]
