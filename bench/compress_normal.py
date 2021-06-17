edc8b251f0c87bd8f20c243ca1b28b3d76169649
jmoore <josh@glencoesoftware.com>
2021-06-17 11:03:05 +0200

    Temporarily disable bench/compress_normal.py

--- bench/compress_normal.py
+++ bench/compress_normal.py
@@ -7,34 +7,36 @@ import line_profiler
 import zarr
 from zarr import blosc
 
-sys.path.insert(0, '..')
+if __name__ == "__main__":
 
-# setup
-a = np.random.normal(2000, 1000, size=200000000).astype('u2')
-z = zarr.empty_like(a, chunks=1000000, compression='blosc', compression_opts=dict(cname='lz4', clevel=5, shuffle=2))
-print(z)
+    sys.path.insert(0, '..')
 
-print('*' * 79)
+    # setup
+    a = np.random.normal(2000, 1000, size=200000000).astype('u2')
+    z = zarr.empty_like(a, chunks=1000000, compression='blosc', compression_opts=dict(cname='lz4', clevel=5, shuffle=2))
+    print(z)
 
-# time
-t = timeit.repeat('z[:] = a', repeat=10, number=1, globals=globals())
-print(t)
-print(min(t))
-print(z)
+    print('*' * 79)
 
-# profile
-profile = line_profiler.LineProfiler(blosc.compress)
-profile.run('z[:] = a')
-profile.print_stats()
+    # time
+    t = timeit.repeat('z[:] = a', repeat=10, number=1, globals=globals())
+    print(t)
+    print(min(t))
+    print(z)
 
-print('*' * 79)
+    # profile
+    profile = line_profiler.LineProfiler(blosc.compress)
+    profile.run('z[:] = a')
+    profile.print_stats()
 
-# time
-t = timeit.repeat('z[:]', repeat=10, number=1, globals=globals())
-print(t)
-print(min(t))
+    print('*' * 79)
 
-# profile
-profile = line_profiler.LineProfiler(blosc.decompress)
-profile.run('z[:]')
-profile.print_stats()
+    # time
+    t = timeit.repeat('z[:]', repeat=10, number=1, globals=globals())
+    print(t)
+    print(min(t))
+
+    # profile
+    profile = line_profiler.LineProfiler(blosc.decompress)
+    profile.run('z[:]')
+    profile.print_stats()
