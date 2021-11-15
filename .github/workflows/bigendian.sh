c0ef6ac672ecfe6cacab7c2a4df90580d494d73c
jmoore <josh@glencoesoftware.com>
2021-11-15 14:52:39 +0100

    Fix python3 command

--- .github/workflows/bigendian.sh
+++ .github/workflows/bigendian.sh
@@ -11,7 +11,7 @@ apt-get update -y
 apt-get install -y python3
 python3 --version
 python3 -c "import platform; print(platform.machine())"
-python -c "import sys;sys.exit(0 if sys.byteorder=='big' else 1)"
+python3 -c "import sys;sys.exit(0 if sys.byteorder=='big' else 1)"
 lscpu | grep Endian
 echo -n "Test endianness: 0=Big, 1=Little ? "
 echo -n I | od -to2 | head -n1 | cut -f2 -d" " | cut -c6
