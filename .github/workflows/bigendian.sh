f4e077e606e9e2db01de290e9428c7e9024bce7c
jmoore <josh@glencoesoftware.com>
2021-11-15 14:59:15 +0100

    Use docker commands from #858

--- .github/workflows/bigendian.sh
+++ .github/workflows/bigendian.sh
@@ -8,10 +8,19 @@ set -x
 # see: https://github.community/t/testing-against-multiple-architectures/17111/6
 #
 apt-get update -y
-apt-get install -y python3
-python3 --version
-python3 -c "import platform; print(platform.machine())"
-python3 -c "import sys;sys.exit(0 if sys.byteorder=='big' else 1)"
+apt-get install -y \
+    python3 \
+    python3-setuptools \
+    python3-pip \
+    python3-pytest \
+    python3-msgpack \
+    python3-fsspec \
+    python3-numcodecs \
+    python3-asciitree \
+    python3-fasteners
+echo =================================
 lscpu | grep Endian
-echo -n "Test endianness: 0=Big, 1=Little ? "
-echo -n I | od -to2 | head -n1 | cut -f2 -d" " | cut -c6
+echo =================================
+
+python -m pip install .
+pytest -sv
