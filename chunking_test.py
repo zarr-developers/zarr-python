80712680ab699fdb7fa5e96977f2a709c505a8a7
Jonathan Striebel <jonathan.striebel@gmail.com>
2021-11-26 13:55:44 +0100

    add chunking_test.py output to itself

--- chunking_test.py
+++ chunking_test.py
@@ -11,11 +11,37 @@ z[19, 2] = 1
 z[0, 1] = -4.2
 
 print(store[".zarray"].decode())
-print("ONDISK", sorted(os.listdir("data/chunking_test.zarr")))
+# {
+#     "chunks": [
+#         3,
+#         2
+#     ],
+#     "compressor": null,
+#     "dtype": "<f8",
+#     "fill_value": 0.0,
+#     "filters": null,
+#     "order": "C",
+#     "shape": [
+#         20,
+#         3
+#     ],
+#     "shard_format": "morton_order",
+#     "shards": [
+#         2,
+#         2
+#     ],
+#     "zarr_format": 2
+# }
+
 assert json.loads(store[".zarray"].decode()) ["shards"] == [2, 2]
 
+print("ONDISK", sorted(os.listdir("data/chunking_test.zarr")))
 print("STORE", sorted(store))
 print("CHUNKSTORE (SHARDED)", sorted(z.chunk_store))
+
+# ONDISK ['.zarray', '0.0', '1.0', '2.0', '3.0']
+# STORE ['.zarray', '0.0', '1.0', '2.0', '3.0']
+# CHUNKSTORE (SHARDED) ['.zarray', '0.0', '0.1', '1.0', '1.1', '2.0', '2.1', '3.0', '3.1', '5.0', '6.1']
 
 z_reopened = zarr.open("data/chunking_test.zarr")
 assert z_reopened.shards == (2, 2)
