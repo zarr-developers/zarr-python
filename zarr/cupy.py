40d084e55df39c93401e09e5224778960b4024eb
Mads R. B. Kristensen <madsbk@gmail.com>
2022-01-11 14:02:42 +0100

    CuPyCPUCompressor: clean up and doc

--- zarr/cupy.py
+++ zarr/cupy.py
@@ -5,30 +5,37 @@ from .util import ensure_contiguous_ndarray
 
 
 class CuPyCPUCompressor(Codec):
+    """CPU compressor for CuPy arrays
+
+    This compressor converts CuPy arrays host memory before compressing
+    the arrays using `compressor`.
+
+    Parameters
+    ----------
+    compressor : numcodecs.abc.Codec
+        The codec to use for compression and decompression.
+    """
+
     codec_id = "cupy_cpu_compressor"
 
-    def __init__(self, cpu_compressor: Codec = None):
-        self.cpu_compressor = cpu_compressor
+    def __init__(self, compressor: Codec = None):
+        self.compressor = compressor
 
     def encode(self, buf):
         import cupy
 
         buf = cupy.asnumpy(ensure_contiguous_ndarray(buf))
-        if self.cpu_compressor:
-            buf = self.cpu_compressor.encode(buf)
+        if self.compressor:
+            buf = self.compressor.encode(buf)
         return buf
 
     def decode(self, chunk, out=None):
         import cupy
 
-        if out is not None:
-            cpu_out = cupy.asnumpy(out)
-        else:
-            cpu_out = None
-        if self.cpu_compressor:
-            chunk = self.cpu_compressor.decode(chunk, cpu_out)
-            if out is None:
-                cpu_out = chunk
+        if self.compressor:
+            cpu_out = None if out is None else cupy.asnumpy(out)
+            chunk = self.compressor.decode(chunk, cpu_out)
+
         chunk = cupy.asarray(ensure_contiguous_ndarray(chunk))
         if out is not None:
             cupy.copyto(out, chunk.view(dtype=out.dtype), casting="no")
@@ -36,17 +43,17 @@ class CuPyCPUCompressor(Codec):
         return chunk
 
     def get_config(self):
-        cc_config = self.cpu_compressor.get_config() if self.cpu_compressor else None
+        cc_config = self.compressor.get_config() if self.compressor else None
         return {
             "id": self.codec_id,
-            "cpu_compressor_config": cc_config,
+            "compressor_config": cc_config,
         }
 
     @classmethod
     def from_config(cls, config):
-        cc_config = config.get("cpu_compressor_config", None)
-        cpu_compressor = get_codec(cc_config) if cc_config else None
-        return cls(cpu_compressor=cpu_compressor)
+        cc_config = config.get("compressor_config", None)
+        compressor = get_codec(cc_config) if cc_config else None
+        return cls(compressor=compressor)
 
 
 register_codec(CuPyCPUCompressor)
