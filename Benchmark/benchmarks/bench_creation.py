1a961d94a9b43544e3c70c5da4da2ca7db81e630
Parth Tripathi <parthdon03@gmail.com>
2022-07-10 12:24:10 +0530

    creation.py update

---
+++ Benchmark/benchmarks/bench_creation.py
@@ -0,0 +1,25 @@
+"""
+ ASV benchmarks for creation.py
+ """
+
+import zarr
+import numpy as np 
+
+
+class creation:
+    
+    def setup(self):
+        self.shape = np.zeros(shape=(3, 2))
+        self.chunks = (1000, 1000)
+        self.dtype = i4
+        
+    
+    def time_create(self):
+        creation.create(self.shape, self.chunks, self.dtype)
+        
+    def time_empty(self):
+        creation.empty(self.shape)
+        
+    
+    def time_full(self):
+        creation.full(self.shape)
