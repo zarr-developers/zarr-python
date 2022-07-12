e3a76c655cadc66f1db64c17a88cc0ccbf917750
Parth Tripathi <86532127+parthxtripathi@users.noreply.github.com>
2022-07-12 16:40:47 +0530

    Update bench_creation.py

--- Benchmark/benchmarks/bench_creation.py
+++ Benchmark/benchmarks/bench_creation.py
@@ -1,26 +1,35 @@
 """
  ASV benchmarks for creation.py
  """
-
 import zarr
-import numpy as np 
-
+import numpy as np
 
 class creation:
     
-    def setup(self):
-        self.shape = np.array((3, 2))
-        self.chunks = np.array((1000, 1000))
-        self.dtype = "i4"
-        self.fill_value = 1 
-        
+    params = [[3, 3], [1000, 1000]]
     
-    def time_create(self):
-        creation.create(self.shape, self.chunks, self.dtype)
-        
-    def time_empty(self):
-        creation.empty(self.shape)
-        
+    def time_full(self, *n):
+        self.array = zarr.full(n, fill_value = 2)
     
-    def time_full(self):
-        creation.full(self.shape, self.fill_value)
+    def time_create(self, *n):
+        self.array = zarr.create(n)
+
+    def time_empty(self, *n):
+        self.array = zarr.empty(n)
+
+    def time_zeros(self, *n):
+        self.array = zarr.zeros(n)
+
+    def time_ones(self, *n):
+        self.array = zarr.ones(n)
+
+if __name__ == "__main__":
+    instance = creation()
+    instance.time_create()
+    print("array", instance.array[:])
+    instance.time_empty()
+    print("array", instance.array[:])
+    instance.time_zeros()
+    print("array", instance.array[:])
+    instance.time_ones()
+    print("array", instance.array[:])
