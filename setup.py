a22f2654555af4c35861fee132d3692f4cad0758
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-10-07 15:22:23 +0200

    Python 3.10 is supported

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     maintainer='Alistair Miles',
     maintainer_email='alimanfoo@googlemail.com',
