948abb6c28d0f9f53993c5ff2f16f60d4e096baa
GbotemiB <gbotemibolarinwa@gmail.com>
2022-10-28 16:34:02 +0100

    ipython fix

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,6 @@ sys.path.append(os.path.abspath('..'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
-import IPython
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
