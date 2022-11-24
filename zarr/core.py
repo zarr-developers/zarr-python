d1594cee857eeb06c38d10735d2ee6d90cc3af92
h20200051 <ankit.aggarwal289@gmail.com>
2022-11-24 14:02:52 +0530

    issue fixed

--- zarr/core.py
+++ zarr/core.py
@@ -2245,87 +2245,6 @@ class Array:
         r += '>'
         return r
 
-# dsize = (60, 404, 802)
-# dchunks = (12, 80, 160)
-# dvalue = 42.
-# docstr = "r"
-# print(docstr)
-
-def zarr_chunk_slice_info():
-
-
-    """Use pure zarr insides to get chunk/slice info."""
-    ds = zarr.open("./example.zarr")
-    print("Zarr stuffs")
-    print("==================")
-    print(f"Data file loaded by Zarr \n: {ds}")
-    print(f"Info of Data file loaded by Zarr\n: {ds.info}")
-    # print(f"Data array loaded by Zarr\n: {ds[:]}")
-    print(f"Data chunks: {ds.chunks}")
-
-    # Zarr chunking information
-    # from zarr.convenience._copy(); convenience module l.897
-    # https://zarr.readthedocs.io/en/stable/api/convenience.html#zarr.convenience.copy
-    shape = ds.shape
-    chunks = ds.chunks
-    chunk_offsets = [range(0, s, c) for s, c in zip(shape, chunks)]
-    print("Chunk offsets", [tuple(k) for k in chunk_offsets])
-    print("Zarr number of chunks", len(list(itertools.product(*chunk_offsets))))
-    offsets = []  # index offsets
-    sels = []  # indices
-    ch_sizes = []  # chunk sizes
-    for offset in itertools.product(*chunk_offsets):
-        offsets.append(offset)
-        sel = tuple(slice(o, min(s, o + c))
-                    for o, s, c in zip(offset, shape, chunks))
-        sels.append(sel)
-        islice = ds[sel]
-        slice_size = islice.size * islice.dtype.itemsize
-        ch_sizes.append(slice_size)
-
-    print("\nAnalyzing 0th and 5th chunks")
-    for j in [0, 5]:
-        print(f"Chunk index {j}")
-        print("Chunk index offset:", offsets[j])
-        print("Chunk position:", sels[j])
-        print("Chunk size:", ch_sizes[j])
-
-    print("\nChunks information")
-    print("Min chunk size:", np.min(ch_sizes))
-    print("Max chunk size:", np.max(ch_sizes))
-    print("Total chunks size COMPRESSED:", np.sum(ch_sizes))
-
-    tot_size = ds.size * ds.dtype.itemsize
-    print(f"\nTotal size (sum of chunks), COMPRESSED: {tot_size}")
-
-    # slice this cake
-    print("\nNow looking at some slices:")
-    data_slice = ds[0:2]  # zarr data slice
-    print(f"Slice Dataset[0:2] shape {data_slice.shape}")
-    PCI = PartialChunkIterator((slice(0, 2, 1), ), ds.shape)
-    print("Slice offset and size", list(PCI)[0][0], "and",
-          list(PCI)[0][1] * ds.dtype.itemsize)
-    print("\n")
-    data_slice = ds[4:7]  # zarr data slice
-    print(f"Slice Dataset[4:7] shape {data_slice.shape}")
-    PCI = PartialChunkIterator((slice(4, 7, 1), ), ds.shape)
-    print("Slice offset and size", list(PCI)[0][0], "and",
-          list(PCI)[0][1] * ds.dtype.itemsize)
-    print("\n")
-    data_slice = ds[0:60]  # the whole cake
-    print(f"Slice Dataset[0:60] shape {data_slice.shape}")
-    PCI = PartialChunkIterator((slice(0, 60, 1), ), ds.shape)
-    print("Slice offset and size", list(PCI)[0][0], "and",
-          list(PCI)[0][1] * ds.dtype.itemsize)
-    print("\n")
-
-
-def main():
-    zarr_chunk_slice_info()
-
-if __name__ == '__main__':
-    main()
-
     @property
     def info(self):
         """Report some diagnostic information about the array.
@@ -2347,9 +2266,78 @@ if __name__ == '__main__':
         No. bytes stored   : 320
         Storage ratio      : 12500.0
         Chunks initialized : 0/10
-
         """
-        return self._info_reporter
+# dsize = (60, 404, 802)
+# dchunks = (12, 80, 160)
+# dvalue = 42.
+# docstr = "r"
+# print(docstr)
+     """Use pure zarr insides to get chunk/slice info."""
+        ds = zarr.open("./example.zarr")
+        print("Zarr stuffs")
+        print("==================")
+        print(f"Data file loaded by Zarr \n: {ds}")
+        print(f"Info of Data file loaded by Zarr\n: {ds.info}")
+        # print(f"Data array loaded by Zarr\n: {ds[:]}")
+        print(f"Data chunks: {ds.chunks}")
+
+    # Zarr chunking information
+    # from zarr.convenience._copy(); convenience module l.897
+    # https://zarr.readthedocs.io/en/stable/api/convenience.html#zarr.convenience.copy
+        shape = ds.shape
+        chunks = ds.chunks
+        chunk_offsets = [range(0, s, c) for s, c in zip(shape, chunks)]
+        print("Chunk offsets", [tuple(k) for k in chunk_offsets])
+        print("Zarr number of chunks", len(list(itertools.product(*chunk_offsets))))
+        offsets = []  # index offsets
+        sels = []  # indices
+        ch_sizes = []  # chunk sizes
+        for offset in itertools.product(*chunk_offsets):
+            offsets.append(offset)
+            sel = tuple(slice(o, min(s, o + c))
+                        for o, s, c in zip(offset, shape, chunks))
+            sels.append(sel)
+            islice = ds[sel]
+            slice_size = islice.size * islice.dtype.itemsize
+            ch_sizes.append(slice_size)
+
+        print("\nAnalyzing 0th and 5th chunks")
+        for j in [0, 5]:
+            print(f"Chunk index {j}")
+            print("Chunk index offset:", offsets[j])
+            print("Chunk position:", sels[j])
+            print("Chunk size:", ch_sizes[j])
+
+        print("\nChunks information")
+        print("Min chunk size:", np.min(ch_sizes))
+        print("Max chunk size:", np.max(ch_sizes))
+        print("Total chunks size COMPRESSED:", np.sum(ch_sizes))
+
+        tot_size = ds.size * ds.dtype.itemsize
+        print(f"\nTotal size (sum of chunks), COMPRESSED: {tot_size}")
+
+    # slice this cake
+        print("\nNow looking at some slices:")
+        data_slice = ds[0:2]  # zarr data slice
+        print(f"Slice Dataset[0:2] shape {data_slice.shape}")
+        PCI = PartialChunkIterator((slice(0, 2, 1), ), ds.shape)
+        print("Slice offset and size", list(PCI)[0][0], "and",
+            list(PCI)[0][1] * ds.dtype.itemsize)
+        print("\n")
+        data_slice = ds[4:7]  # zarr data slice
+        print(f"Slice Dataset[4:7] shape {data_slice.shape}")
+        PCI = PartialChunkIterator((slice(4, 7, 1), ), ds.shape)
+        print("Slice offset and size", list(PCI)[0][0], "and",
+            list(PCI)[0][1] * ds.dtype.itemsize)
+        print("\n")
+        data_slice = ds[0:60]  # the whole cake
+        print(f"Slice Dataset[0:60] shape {data_slice.shape}")
+        PCI = PartialChunkIterator((slice(0, 60, 1), ), ds.shape)
+        print("Slice offset and size", list(PCI)[0][0], "and",
+            list(PCI)[0][1] * ds.dtype.itemsize)
+        print("\n")
+
+        # return self._info_reporter
 
     def info_items(self):
         return self._synchronized_op(self._info_items_nosync)
