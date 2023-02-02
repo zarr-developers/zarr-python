4dc6f1f5046708648b5e6b82d0a64a24d1a40566
AWA BRANDON AWA <brandonawa78@gmail.com>
2023-02-02 16:01:58 +0100

    changed documentation theme to pydata_sphinx_theme (#1242)
    * changed documentation theme to pydata_sphinx_theme
    
    * updated documentation layout
    
    * Update docs/index.rst
    
    Co-authored-by: Sanket Verma <svsanketverma5@gmail.com>
    
    * Updated Acknowledgements section and added twitter icon
    
    * Added client-side javascript redirects
    
    * Add more redirects
    
    * Minor tweaks and added acknowledgments.rst
    
    * Added acknowledgments.html to redirect
    
    * Add indices from the old main page
    
    ---------
    
    Co-authored-by: Sanket Verma <svsanketverma5@gmail.com>
    Co-authored-by: Josh Moore <josh@openmicroscopy.org>

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'numpydoc',
     'sphinx_issues',
     "sphinx_copybutton",
+    "sphinx_design"
 ]
 
 numpydoc_show_class_members = False
@@ -124,12 +125,26 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
+
+html_favicon = '_static/logo1.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'logo_only': True}
+html_theme_options = {
+  "github_url": "https://github.com/zarr-developers/zarr-python",
+  "twitter_url": "https://twitter.com/zarr_dev",
+  "icon_links": [
+    {
+        "name": "Zarr Dev",
+        "url": "https://zarr.dev/",
+        "icon": "_static/logo1.png",
+        "type": "local"
+    },
+  ],
+  "collapse_navigation": True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -160,6 +175,9 @@ def setup(app):
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_js_files = [
+    'custom.js',
+]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -246,7 +264,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (main_doc, 'zarr.tex', 'zarr Documentation',
+    (main_doc, 'zarr.tex', 'Zarr-Python',
      author, 'manual'),
 ]
 
@@ -276,7 +294,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (main_doc, 'zarr', 'zarr Documentation',
+    (main_doc, 'zarr', 'Zarr-Python',
      [author], 1)
 ]
 
@@ -290,7 +308,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (main_doc, 'zarr', 'zarr Documentation',
+    (main_doc, 'zarr', 'Zarr-Python',
      author, 'zarr', 'One line description of project.',
      'Miscellaneous'),
 ]
