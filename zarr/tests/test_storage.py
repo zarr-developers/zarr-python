1b89bf3670340ebf3296e7152ea3fabb6d0decf8
Ian Carroll <ian.t.carroll@nasa.gov>
2023-12-08 11:30:43 -0500

    add missing argument in tests, lint

--- zarr/tests/test_storage.py
+++ zarr/tests/test_storage.py
@@ -1341,14 +1341,13 @@ class TestFSStore(StoreTests):
         import fsspec
         m = fsspec.filesystem("memory")
         g = zarr.open_group("memory://test/out.zarr", mode='w')
-        arr = g.create_dataset("data", data=[1, 2, 3, 4],
-                               dtype="i4", compression=None, chunks=[2])
+        arr = g.create_dataset("data", data=[1, 2, 3, 4], dtype="i4", compression=None, chunks=[2])
         m.store["/test/out.zarr/data/0"] = None
         del m.store["/test/out.zarr/data/1"]
-        assert g.store.getitems(["data/1"]) == {}  # not found
+        assert g.store.getitems(["data/1"], contexts={}) == {}  # not found
         with pytest.raises(Exception):
             # None is bad data, as opposed to missing
-            g.store.getitems(["data/0", "data/1"])
+            g.store.getitems(["data/0", "data/1"], contexts={})
         with pytest.raises(Exception):
             # None is bad data, as opposed to missing
             arr[:]
