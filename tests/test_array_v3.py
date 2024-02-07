0c37a210253504d72792a10d073bf8dec0577285
Ryan Abernathey <ryan.abernathey@gmail.com>
2024-02-07 14:31:49 -0500

    added creation from group

--- tests/test_array_v3.py
+++ tests/test_array_v3.py
@@ -1,13 +1,31 @@
 from zarr.v3 import Array
+from zarr.v3.group import Group
 from zarr.v3.store import MemoryStore
 
 
-def test_create_array_v3():
+def test_create_array():
 
     store = MemoryStore()
     _ = Array.create(
         store,
         shape=(100,),
+        chunk_shape=(100,),
+        dtype="int32",
+    )
+
+
+def test_create_array_from_group():
+    store = MemoryStore()
+    group = Group.create(store)
+    _a = group.create_array(
+        "a",
+        shape=(100,),
+        chunk_shape=(100,),
+        dtype="int32",
+    )
+    _b = group.create_array(
+        "b",
+        shape=(200,),
         chunk_shape=(100,),
         dtype="int32",
     )
