29fa0a46c2f9b36097077be885e4554d566a35cc
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-04-12 14:06:30 -0400

    upath to be optional

--- src/zarr/v3/store/remote.py
+++ src/zarr/v3/store/remote.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Union, List
 
+import fsspec
 from zarr.v3.abc.store import Store
 from zarr.v3.store.core import _dereference_path
 from zarr.v3.common import BytesLike
@@ -17,26 +18,41 @@ class FsspecStore(Store):
     supports_partial_writes: bool = False
     supports_listing: bool = True
 
-    root: UPath
+    _fs: AsyncFileSystem
+    exceptions = tuple[type]
 
-    def __init__(self, url: Union[UPath, str], **storage_options: Dict[str, Any]):
-        from upath import UPath
+    def __init__(
+            self,
+            url: Union[UPath, str],
+            allowed_exceptions: tuple[type] = (FileNotFoundError, IsADirectoryError, NotADirectoryError),
+            **storage_options: Any
+    ):
+        """
+        Parameters
+        ----------
+        url: root of the datastore. In fsspec notation, this is usually like "protocol://path/to".
+            Can also be a upath.UPath instance/
+        allowed_exceptions: when fetching data, these cases will be deemed to correspond to missinf
+            keys, rather than some other IO failure
+        storage_options: passed on to fsspec to make the filesystem instance. If url is a UPath,
+            this must not be used.
+        """
 
         if isinstance(url, str):
-            self.root = UPath(url, **storage_options)
-        else:
+            self._fs, self.path = fsspec.url_to_fs(url, **storage_options)
+        elif hasattr(u, "protocol") and hasattr(u, "fs"):
+            # is UPath-liks
             assert len(storage_options) == 0, (
                 "If constructed with a UPath object, no additional "
                 + "storage_options are allowed."
             )
-            self.root = url
-        self.path = self.root.path
+            self.path = url.path
+            self._fs = url.fs
+        else:
+            raise ValueError("URL not understood, %s", url)
+        self.exceptions = allowed_exceptions
         # test instantiate file system
-        assert self.root.fs.async_impl, "FileSystem needs to support async operations."
-
-    @property
-    def _fs(self):
-        return self.root.fs
+        assert self._fs.async_impl, "FileSystem needs to support async operations."
 
     def __str__(self) -> str:
         return f"Remote fsspec store: {self.root}"
@@ -56,7 +72,7 @@ class FsspecStore(Store):
                 if byte_range
                 else self._fs._cat_file(path)
             )
-        except (FileNotFoundError, IsADirectoryError, NotADirectoryError):
+        except self.exceptions:
             return None
 
         return value
@@ -77,8 +93,10 @@ class FsspecStore(Store):
 
     async def delete(self, key: str) -> None:
         path = _dereference_path(self.path, key)
-        if await self._fs._exists(path):
+        try:
             await self._fs._rm(path)
+        except FileNotFoundError + self.exceptions:
+            pass
 
     async def exists(self, key: str) -> bool:
         path = _dereference_path(self.path, key)
@@ -89,4 +107,5 @@ class FsspecStore(Store):
             (_dereference_path(self.path, k[0]), k[1][0], k[1][1])
             for k in key_ranges
         ]
-        return await self._cat_ranges(paths, starts, stops)
+        # TODO: expectations for exceptions or missing keys?
+        return await self._cat_ranges(paths, starts, stops, on_error="return")
