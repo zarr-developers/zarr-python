320a6711f747655f8fdc03a86f73feeaa71f8643
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-04-12 21:40:26 -0400

    fill out methods

--- src/zarr/v3/store/remote.py
+++ src/zarr/v3/store/remote.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Union, List
 
 import fsspec
 from zarr.v3.abc.store import Store
-from zarr.v3.store.core import _dereference_path
+from zarr.v3.store.core import _dereference_path, make_store_path
 from zarr.v3.common import BytesLike
 
 
@@ -41,13 +41,13 @@ class FsspecStore(Store):
         if isinstance(url, str):
             self._fs, self.path = fsspec.url_to_fs(url, **storage_options)
         elif hasattr(u, "protocol") and hasattr(u, "fs"):
-            # is UPath-liks
+            # is UPath-like - but without importing
             assert len(storage_options) == 0, (
                 "If constructed with a UPath object, no additional "
                 + "storage_options are allowed."
             )
             self.path = url.path
-            self._fs = url.fs
+            self._fs = url._fs
         else:
             raise ValueError("URL not understood, %s", url)
         self.exceptions = allowed_exceptions
@@ -58,16 +58,15 @@ class FsspecStore(Store):
         return f"Remote fsspec store: {self.root}"
 
     def __repr__(self) -> str:
-        return f"<FsspecStore({self})>"
+        return f"<FsspecStore({self.root})>"
 
     async def get(
         self, key: str, byte_range: Optional[Tuple[int, Optional[int]]] = None
     ) -> Optional[BytesLike]:
-        assert isinstance(key, str)
         path = _dereference_path(self.path, key)
 
         try:
-            value = await (
+            return await (
                 self._fs._cat_file(path, start=byte_range[0], end=byte_range[1])
                 if byte_range
                 else self._fs._cat_file(path)
@@ -75,21 +74,15 @@ class FsspecStore(Store):
         except self.exceptions:
             return None
 
-        return value
-
     async def set(
         self, key: str, value: BytesLike, byte_range: Optional[Tuple[int, int]] = None
     ) -> None:
-        assert isinstance(key, str)
         path = _dereference_path(self.path, key)
 
         # write data
         if byte_range:
-            with self._fs._open(path, "r+b") as f:
-                f.seek(byte_range[0])
-                f.write(value)
-        else:
-            await self._fs._pipe_file(path, value)
+            raise NotImplementedError
+        await self._fs._pipe_file(path, value)
 
     async def delete(self, key: str) -> None:
         path = _dereference_path(self.path, key)
@@ -102,10 +95,25 @@ class FsspecStore(Store):
         path = _dereference_path(self.path, key)
         return await self._fs._exists(path)
 
+    async def list(self) -> List[str]:
+        allfiles = await self._fs._find(self.path, detail=False, withdirs=False)
+        return [a.replace(self.path + "/", "") for a in allfiles]
+
+    async def list_dir(self, prefix: str) -> List[str]:
+        prefix = prefix.rstrip("/")
+        allfiles = await self._fs._ls(prefix, detail=False)
+        return [a.replace(prefix + "/", "") for a in allfiles]
+
+    async def list_prefix(self, prefix: str) -> List[str]:
+        return await self._fs._ls(prefix, detail=False)
+
+    async def set_partial_values(self, key_start_values: List[Tuple[str, int, bytes]]) -> None:
+        raise NotImplementedError
+
     async def get_partial_values(self, key_ranges: List[Tuple[str, Tuple[int, int]]]) -> List[bytes]:
         paths, starts, stops = [
             (_dereference_path(self.path, k[0]), k[1][0], k[1][1])
             for k in key_ranges
         ]
         # TODO: expectations for exceptions or missing keys?
-        return await self._cat_ranges(paths, starts, stops, on_error="return")
+        return await self._fs._cat_ranges(paths, starts, stops, on_error="return")
