99a1f93b2a9bab071e9238fae9edd5688a130c81
Norman Rzepka <code@normanrz.com>
2024-05-13 22:15:03 +0200

    use zarr.config for batch_size

--- tests/v3/test_config.py
+++ tests/v3/test_config.py
@@ -4,7 +4,11 @@ from zarr.config import config
 def test_config_defaults_set():
     # regression test for available defaults
     assert config.defaults == [
-        {"array": {"order": "C"}, "async": {"concurrency": None, "timeout": None}}
+        {
+            "array": {"order": "C"},
+            "async": {"concurrency": None, "timeout": None},
+            "codec_pipeline": {"batch_size": 1000},
+        }
     ]
     assert config.get("array.order") == "C"
 
