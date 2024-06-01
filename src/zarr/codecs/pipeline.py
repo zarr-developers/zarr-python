e2a4aff1fc73abd7f206396e8aa25ff4351a1053
Norman Rzepka <code@normanrz.com>
2024-06-01 20:59:20 +0200

    pr feedback

--- src/zarr/codecs/pipeline.py
+++ src/zarr/codecs/pipeline.py
@@ -333,6 +333,44 @@ class BatchedCodecPipeline(CodecPipeline):
                 else:
                     out[out_selection] = chunk_spec.fill_value
 
+    def _merge_chunk_array(
+        self,
+        existing_chunk_array: NDBuffer | None,
+        value: NDBuffer,
+        out_selection: SelectorTuple,
+        chunk_spec: ArraySpec,
+        chunk_selection: SelectorTuple,
+        drop_axes: tuple[int, ...],
+    ) -> NDBuffer:
+        if is_total_slice(chunk_selection, chunk_spec.shape) and value.shape == chunk_spec.shape:
+            return value
+        if existing_chunk_array is None:
+            chunk_array = NDBuffer.create(
+                shape=chunk_spec.shape,
+                dtype=chunk_spec.dtype,
+                order=chunk_spec.order,
+                fill_value=chunk_spec.fill_value,
+            )
+        else:
+            chunk_array = existing_chunk_array.copy()  # make a writable copy
+        if chunk_selection == ():
+            chunk_value = value
+        elif is_scalar(value.as_ndarray_like(), chunk_spec.dtype):
+            chunk_value = value
+        else:
+            chunk_value = value[out_selection]
+            # handle missing singleton dimensions
+            if drop_axes != ():
+                item = tuple(
+                    None  # equivalent to np.newaxis
+                    if idx in drop_axes
+                    else slice(None)
+                    for idx in range(chunk_spec.ndim)
+                )
+                chunk_value = chunk_value[item]
+        chunk_array[chunk_selection] = chunk_value
+        return chunk_array
+
     async def write_batch(
         self,
         batch_info: Iterable[tuple[ByteSetter, ArraySpec, SelectorTuple, SelectorTuple]],
@@ -372,48 +410,8 @@ class BatchedCodecPipeline(CodecPipeline):
                 ],
             )
 
-            def _merge_chunk_array(
-                existing_chunk_array: NDBuffer | None,
-                value: NDBuffer,
-                out_selection: SelectorTuple,
-                chunk_spec: ArraySpec,
-                chunk_selection: SelectorTuple,
-                drop_axes: tuple[int, ...],
-            ) -> NDBuffer:
-                if (
-                    is_total_slice(chunk_selection, chunk_spec.shape)
-                    and value.shape == chunk_spec.shape
-                ):
-                    return value
-                if existing_chunk_array is None:
-                    chunk_array = NDBuffer.create(
-                        shape=chunk_spec.shape,
-                        dtype=chunk_spec.dtype,
-                        order=chunk_spec.order,
-                        fill_value=chunk_spec.fill_value,
-                    )
-                else:
-                    chunk_array = existing_chunk_array.copy()  # make a writable copy
-                if chunk_selection == ():
-                    chunk_value = value
-                elif is_scalar(value.as_ndarray_like(), chunk_spec.dtype):
-                    chunk_value = value
-                else:
-                    chunk_value = value[out_selection]
-                    # handle missing singleton dimensions
-                    if drop_axes != ():
-                        item = tuple(
-                            None  # equivalent to np.newaxis
-                            if idx in drop_axes
-                            else slice(None)
-                            for idx in range(chunk_spec.ndim)
-                        )
-                        chunk_value = chunk_value[item]
-                chunk_array[chunk_selection] = chunk_value
-                return chunk_array
-
             chunk_array_batch = [
-                _merge_chunk_array(
+                self._merge_chunk_array(
                     chunk_array, value, out_selection, chunk_spec, chunk_selection, drop_axes
                 )
                 for chunk_array, (_, chunk_spec, chunk_selection, out_selection) in zip(
