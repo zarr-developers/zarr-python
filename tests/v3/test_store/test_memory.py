36b1cb277a8d6b3a7eb9521e13b701b984fde110
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2024-06-13 22:33:37 -0700

    Adding GPU memory store implementation

--- tests/v3/test_store/test_memory.py
+++ tests/v3/test_store/test_memory.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import pytest
 
-from zarr.buffer import Buffer
-from zarr.store.memory import MemoryStore
+from zarr.buffer import Buffer, GpuBuffer
+from zarr.store.memory import GpuMemoryStore, MemoryStore
 from zarr.testing.store import StoreTests
 
 
@@ -37,4 +37,37 @@ class TestMemoryStore(StoreTests[MemoryStore]):
         assert store.supports_partial_writes
 
     def test_list_prefix(self, store: MemoryStore) -> None:
+        assert True
+
+
+class TestGpuMemoryStore(StoreTests[GpuMemoryStore]):
+    store_cls = GpuMemoryStore
+
+    def set(self, store: GpuMemoryStore, key: str, value: Buffer) -> None:
+        store._store_dict[key] = value
+
+    def get(self, store: MemoryStore, key: str) -> Buffer:
+        return store._store_dict[key]
+
+    @pytest.fixture(scope="function", params=[None, {}])
+    def store_kwargs(self, request) -> dict[str, str | None | dict[str, Buffer]]:
+        return {"store_dict": request.param, "mode": "w"}
+
+    @pytest.fixture(scope="function")
+    def store(self, store_kwargs: str | None | dict[str, GpuBuffer]) -> GpuMemoryStore:
+        return self.store_cls(**store_kwargs)
+
+    def test_store_repr(self, store: GpuMemoryStore) -> None:
+        assert str(store) == f"gpumemory://{id(store._store_dict)}"
+
+    def test_store_supports_writes(self, store: GpuMemoryStore) -> None:
+        assert store.supports_writes
+
+    def test_store_supports_listing(self, store: GpuMemoryStore) -> None:
+        assert store.supports_listing
+
+    def test_store_supports_partial_writes(self, store: GpuMemoryStore) -> None:
+        assert store.supports_partial_writes
+
+    def test_list_prefix(self, store: GpuMemoryStore) -> None:
         assert True
