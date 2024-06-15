a94b9952e5347f064e89a58989ba48c6aed241d4
brokkoli71 <brokkoli71b@gmail.com>
2024-06-16 00:08:13 +0200

    add test_orthogonal_bool_indexing_like_numpy_ix

--- tests/v3/test_indexing.py
+++ tests/v3/test_indexing.py
@@ -1729,28 +1729,41 @@ def test_accessed_chunks(shape, chunks, ops):
     "selection",
     [
         # basic selection
-        ...,
-        (1, ...),
-        slice(None),
-        (1,3),
-        ([1, 2, 3],9),
-        np.arange(1000),
-        slice(5, 15),
-        (slice(2, 4), 4),
+        [...],
+        [1, ...],
+        [slice(None)],
         [1,3],
+        [(1,3)],
+        [[1, 2, 3],9],
+        [np.arange(1000)],
+        [slice(5, 15)],
+        [slice(2, 4), 4],
+        [[1,3]],
         # mask selection
-        np.tile([True, False], (1000,5)),
-        np.full((1000, 10), False),
+        [np.tile([True, False], (1000,5))],
+        [np.full((1000, 10), False)],
         # coordinate selection
-        ([1, 2, 3, 4], [5, 6, 7, 8]),
-        ([100, 200, 300], [4, 5, 6]),
-        # orthogonal selection
-        (np.tile([True, False], 500), np.tile([True, False], 5)),
-    ])
+        [[1, 2, 3, 4], [5, 6, 7, 8]],
+        [[100, 200, 300], [4, 5, 6]],
+])
 def test_indexing_equals_numpy(store, selection):
     a = np.arange(10000, dtype=int).reshape(1000, 10)
     z = zarr_array_from_numpy_array(store, a, chunk_shape=(300, 3))
-    expected = a[selection]
-    actual = z[selection]
+    expected = a[*selection]
+    actual = z[*selection]
     assert_array_equal(expected, actual, err_msg=f"selection: {selection}")
-    z.oindex
+
+@pytest.mark.parametrize(
+    "selection",
+    [
+        [np.tile([True, False], 500), np.tile([True, False], 5)],
+        [np.full(1000, False), np.tile([True, False], 5)],
+        [np.full(1000, True), np.full(10, True)],
+        [np.full(1000, True), [True, False]*5],
+    ])
+def test_orthogonal_bool_indexing_like_numpy_ix(store, selection):
+    a = np.arange(10000, dtype=int).reshape(1000, 10)
+    z = zarr_array_from_numpy_array(store, a, chunk_shape=(300, 3))
+    expected = a[np.ix_(*selection)]
+    actual = z[*selection]
+    assert_array_equal(expected, actual, err_msg=f"{selection=}")
