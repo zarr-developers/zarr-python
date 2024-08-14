d298ed131ee8bb12368f60e144ee9f9924452d53
Emma Marshall <55526386+e-marshall@users.noreply.github.com>
2024-08-13 18:55:30 -0600

    remove bundles

--- tests/v3/test_store/test_stateful_store.py
+++ tests/v3/test_store/test_stateful_store.py
@@ -5,18 +5,14 @@ import asyncio
 import hypothesis.strategies as st
 from hypothesis import note
 from hypothesis.stateful import (
-    Bundle,
     RuleBasedStateMachine,
-    initialize,
     invariant,
     precondition,
     rule,
 )
 
-from zarr.buffer import Buffer, default_buffer_prototype
+from zarr.buffer import Buffer, BufferPrototype, default_buffer_prototype
 from zarr.store import MemoryStore
-
-# from strategies_store import StoreStatefulStrategies, key_ranges
 from zarr.testing.strategies import key_ranges, paths
 
 # zarr spec: https://zarr-specs.readthedocs.io/en/latest/v3/core/v3.0.html
@@ -34,18 +30,19 @@ class SyncStoreWrapper:
         self.store = store
         self.mode = store.mode
 
-    def set(self, key, data_buffer):  # buffer is value
+    def set(self, key, data_buffer):
         return asyncio.run(self.store.set(key, data_buffer))
 
     def list(self):
         async def wrapper(gen):
             return [i async for i in gen]
 
-        gen = self.store.list()  # async store list
-        return (i for i in asyncio.run(wrapper(gen)))
+        gen = self.store.list()
+        yield from asyncio.run(wrapper(gen))
 
-    def get(self, key):
-        obs = asyncio.run(self.store.get(key, prototype=default_buffer_prototype()))
+    def get(self, key, prototype: BufferPrototype):
+        # the input args on this get should match get() from memory store
+        obs = asyncio.run(self.store.get(key, prototype=prototype))
         return obs
 
     def get_partial_values(self, key_ranges):
@@ -56,7 +53,7 @@ class SyncStoreWrapper:
         )
         return obs_partial
 
-    def delete(self, path):  # path is key
+    def delete(self, path):
         return asyncio.run(self.store.delete(path))
 
     def empty(self):
@@ -88,45 +85,39 @@ class SyncStoreWrapper:
 
 
 class ZarrStoreStateMachine(RuleBasedStateMachine):
-    keys_bundle = Bundle("keys_bundle")
-    key_ranges_bundle = Bundle("key_ranges")
-
     def __init__(self):
         super().__init__()
         self.model: dict[str, bytes] = {}
         self.store = SyncStoreWrapper(MemoryStore(mode="w"))
 
-    # NOTE: set this and the next as initialize instead of rule to make sure they run
-    #      and create key, key_ranges before any rules that use key, key_ranges
-    #      not totally sure this is necessary
-    @initialize(key=paths, data=st.binary(min_size=0, max_size=100), target=keys_bundle)
+    @rule(key=paths, data=st.binary(min_size=0, max_size=100))
     def set(self, key: str, data: bytes) -> None:
         note(f"(set) Setting {key!r} with {data}")
         assert not self.store.mode.readonly
         data_buf = Buffer.from_bytes(data)
         self.store.set(key, data_buf)
         self.model[key] = data_buf
-        note("init key")
-        return key
-
-    @initialize(key=keys_bundle, data=st.data(), target=key_ranges_bundle)
-    def init_key_ranges(self, data, key):
-        note("init key ranges")
-        key_st = st.just(key)
-        key_ranges_st = key_ranges(keys=key_st)
-        return data.draw(key_ranges_st)
 
     @precondition(lambda self: len(self.model.keys()) > 0)
-    @rule(key=keys_bundle)
-    def get(self, key) -> None:
-        note(f"(get) {key=}")
-        store_value = self.store.get(key)
-        # to bytes here necessary (on model and store) because data_buf set to model in set()
+    @rule(key=paths, data=st.data(), prototype=st.just(default_buffer_prototype()))
+    def get(self, key, data, prototype) -> None:
+        key = data.draw(st.sampled_from(sorted(self.model.keys())))
+        model_value = self.model[key]
+        note(f"(get) model value {model_value.to_bytes()}")
+        store_value = self.store.get(key, prototype)
+        note(f"(get) store value: {store_value.to_bytes()}")
+        # to bytes here necessary because data_buf set to model in set()
         assert self.model[key].to_bytes() == (store_value.to_bytes())
 
+    # write test for invalid keys
+    # eg @rule() def get_invalid() <- key strategy that can have valid and invalid keys
+    # check that drawn key not in keys, then check that model/store returns None or raises error
+    # look at using assume, assume key not in model keys
+
     @precondition(lambda self: len(self.model.keys()) > 0)
-    @rule(key=keys_bundle, key_range=key_ranges_bundle)
-    def get_partial_values(self, key, key_range) -> None:
+    @rule(data=st.data())
+    def get_partial_values(self, data) -> None:
+        key_range = data.draw(key_ranges(keys=st.sampled_from(sorted(self.model.keys()))))
         note(f"(get partial) {key_range=}")
         obs_maybe = self.store.get_partial_values(key_range)
         observed = []
@@ -142,7 +133,6 @@ class ZarrStoreStateMachine(RuleBasedStateMachine):
             start = byte_range[0] or 0
             step = byte_range[1]
             stop = start + step if step is not None else None
-            assert model_vals[start:stop] is not None
             model_vals_ls.append(model_vals[start:stop])
 
         assert all(
@@ -155,11 +145,11 @@ class ZarrStoreStateMachine(RuleBasedStateMachine):
     @precondition(lambda self: len(self.model.keys()) > 0)
     @rule(data=st.data())
     def delete(self, data) -> None:
-        path_st = data.draw(st.sampled_from(sorted(self.model.keys())))
-        note(f"(delete) Deleting {path_st=}")
+        key = data.draw(st.sampled_from(sorted(self.model.keys())))
+        note(f"(delete) Deleting {key=}")
 
-        self.store.delete(path_st)
-        del self.model[path_st]
+        self.store.delete(key)
+        del self.model[key]
 
     @rule()
     def clear(self):
@@ -167,22 +157,25 @@ class ZarrStoreStateMachine(RuleBasedStateMachine):
         note("(clear)")
         self.store.clear()
         self.model.clear()
-        # check that model was cleared
-        assert len(self.model.keys()) == 0
+
+        # checking that clear did happen, since i don't think this is specifically
+        # checked in invariants, though it should still work
+        assert (
+            len(self.model.keys()) == len(list(self.store.list())) == 0
+        )  # <- make sure this is tested in an invariant
 
     @rule()
     def empty(self) -> None:
         note("(empty)")
 
-        # make sure they either both are or both aren't (same state)
+        # make sure they either both are or both aren't empty (same state)
         assert self.store.empty() == (not self.model)
 
-    @rule(key=keys_bundle)
+    @rule(key=paths)
     def exists(self, key) -> None:
-        #
         note("(exists)")
-        # make sure same state
-        assert self.store.exists(key) == (key in self.model)
+
+        assert self.store.exists(key) == key in self.model
 
     @invariant()
     def check_paths_equal(self) -> None:
@@ -216,7 +209,6 @@ class ZarrStoreStateMachine(RuleBasedStateMachine):
 
             for key in keys:
                 assert self.store.exists(key) is True
-                # assert self.store.empty() is False
         note("checking keys / exists / empty")
 
 
