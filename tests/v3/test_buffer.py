c8c7e6dcd69c203a37448f9375c6dbdb1443ea48
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2024-08-21 11:54:59 -0700

    Formatting

--- tests/v3/test_buffer.py
+++ tests/v3/test_buffer.py
@@ -19,13 +19,13 @@ from zarr.codecs.transpose import TransposeCodec
 from zarr.codecs.zstd import ZstdCodec
 from zarr.store.core import StorePath
 from zarr.store.memory import MemoryStore
-from zarr.testing.utils import gpu_test
 from zarr.testing.buffer import (
     NDBufferUsingTestNDArrayLike,
     StoreExpectingTestBuffer,
     TestBuffer,
     TestNDArrayLike,
 )
+from zarr.testing.utils import gpu_test
 
 try:
     import cupy as cp
@@ -53,9 +53,7 @@ async def test_async_array_prototype():
     )
     expect[1:4, 3:6] = np.ones((3, 3))
 
-    my_prototype = BufferPrototype(
-        buffer=TestBuffer, nd_buffer=NDBufferUsingTestNDArrayLike
-    )
+    my_prototype = BufferPrototype(buffer=TestBuffer, nd_buffer=NDBufferUsingTestNDArrayLike)
 
     await a.setitem(
         selection=(slice(1, 4), slice(3, 6)),
@@ -87,9 +85,7 @@ async def test_async_array_gpu_prototype():
         value=cp.ones((3, 3)),
         prototype=gpu.buffer_prototype,
     )
-    got = await a.getitem(
-        selection=(slice(0, 9), slice(0, 9)), prototype=gpu.buffer_prototype
-    )
+    got = await a.getitem(selection=(slice(0, 9), slice(0, 9)), prototype=gpu.buffer_prototype)
     assert isinstance(got, cp.ndarray)
     assert cp.array_equal(expect, got)
 
@@ -114,18 +110,14 @@ async def test_codecs_use_of_prototype():
     )
     expect[:] = np.arange(100).reshape(10, 10)
 
-    my_prototype = BufferPrototype(
-        buffer=TestBuffer, nd_buffer=NDBufferUsingTestNDArrayLike
-    )
+    my_prototype = BufferPrototype(buffer=TestBuffer, nd_buffer=NDBufferUsingTestNDArrayLike)
 
     await a.setitem(
         selection=(slice(0, 10), slice(0, 10)),
         value=expect[:],
         prototype=my_prototype,
     )
-    got = await a.getitem(
-        selection=(slice(0, 10), slice(0, 10)), prototype=my_prototype
-    )
+    got = await a.getitem(selection=(slice(0, 10), slice(0, 10)), prototype=my_prototype)
     assert isinstance(got, TestNDArrayLike)
     assert np.array_equal(expect, got)
 
@@ -156,17 +148,13 @@ async def test_codecs_use_of_gpu_prototype():
         value=expect[:],
         prototype=gpu.buffer_prototype,
     )
-    got = await a.getitem(
-        selection=(slice(0, 10), slice(0, 10)), prototype=gpu.buffer_prototype
-    )
+    got = await a.getitem(selection=(slice(0, 10), slice(0, 10)), prototype=gpu.buffer_prototype)
     assert isinstance(got, cp.ndarray)
     assert cp.array_equal(expect, got)
 
 
 def test_numpy_buffer_prototype():
     buffer = cpu.buffer_prototype().buffer.create_zero_length()
-    ndbuffer = cpu.buffer_prototype().nd_buffer.create(
-        shape=(1, 2), dtype=np.dtype("int64")
-    )
+    ndbuffer = cpu.buffer_prototype().nd_buffer.create(shape=(1, 2), dtype=np.dtype("int64"))
     assert isinstance(buffer.as_array_like(), np.ndarray)
     assert isinstance(ndbuffer.as_ndarray_like(), np.ndarray)
