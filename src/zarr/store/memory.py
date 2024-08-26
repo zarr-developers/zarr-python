3c845d93c5fd28b18867c4a0a6e6c4ffb089fc8e
Tom Augspurger <tom.w.augspurger@gmail.com>
2024-08-26 12:41:20 -0500

    simplify

--- src/zarr/store/memory.py
+++ src/zarr/store/memory.py
@@ -114,17 +114,15 @@ class MemoryStore(Store):
 
         if prefix == "":
             keys_unique = set(k.split("/")[0] for k in self._store_dict.keys())
-            for key in keys_unique:
-                yield key
         else:
             # Our dictionary doesn't contain directory markers, but we want to include
             # a pseudo directory when there's a nested item and we're listing an
             # intermediate level.
-            n = prefix.count("/") + 2
             keys_unique = {
-                "/".join(k.split("/", n)[:n])
-                for k in self._store_dict
-                if k.startswith(prefix + "/")
+                key.removeprefix(prefix + "/").split("/")[0]
+                for key in self._store_dict
+                if key.startswith(prefix + "/") and key != prefix
             }
-            for key in keys_unique:
-                yield key.removeprefix(prefix + "/").split("/")[0]
+
+        for key in keys_unique:
+            yield key
