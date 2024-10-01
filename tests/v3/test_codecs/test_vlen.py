507161a99721fe3b6ec2e631482ff797899ee225
Ryan Abernathey <ryan.abernathey@gmail.com>
2024-09-30 22:18:10 -0400

    satisfy mypy for tests

--- tests/v3/test_codecs/test_vlen.py
+++ tests/v3/test_codecs/test_vlen.py
@@ -1,16 +1,18 @@
+from typing import Any
+
 import numpy as np
 import pytest
 
 from zarr import Array
 from zarr.abc.store import Store
 from zarr.codecs import VLenUTF8Codec
-from zarr.core.metadata.v3 import DataType
+from zarr.core.metadata.v3 import ArrayV3Metadata, DataType
 from zarr.store.common import StorePath
 
 
 @pytest.mark.parametrize("store", ["memory", "local"], indirect=["store"])
 @pytest.mark.parametrize("dtype", [None, np.dtypes.StrDType])
-async def test_vlen_string(store: Store, dtype) -> None:
+async def test_vlen_string(store: Store, dtype: None | np.dtype[Any]) -> None:
     strings = ["hello", "world", "this", "is", "a", "test"]
     data = np.array(strings).reshape((2, 3))
     if dtype is not None:
@@ -25,6 +27,7 @@ async def test_vlen_string(store: Store, dtype) -> None:
         fill_value="",
         codecs=[VLenUTF8Codec()],
     )
+    assert isinstance(a.metadata, ArrayV3Metadata)  # needed for mypy
 
     a[:, :] = data
     assert np.array_equal(data, a[:, :])
@@ -33,6 +36,7 @@ async def test_vlen_string(store: Store, dtype) -> None:
 
     # test round trip
     b = Array.open(sp)
+    assert isinstance(b.metadata, ArrayV3Metadata)  # needed for mypy
     assert np.array_equal(data, b[:, :])
     assert b.metadata.data_type == DataType.string
     assert b.dtype == np.dtypes.StringDType()
