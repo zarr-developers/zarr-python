a5c2a37470d41dbe49dc091e189dc546332148c6
Ryan Abernathey <ryan.abernathey@gmail.com>
2024-10-04 08:46:19 -0400

    rename module

--- src/zarr/codecs/__init__.py
+++ src/zarr/codecs/__init__.py
@@ -4,10 +4,10 @@ from zarr.codecs.blosc import BloscCname, BloscCodec, BloscShuffle
 from zarr.codecs.bytes import BytesCodec, Endian
 from zarr.codecs.crc32c_ import Crc32cCodec
 from zarr.codecs.gzip import GzipCodec
-from zarr.codecs.legacy_vlen import VLenUTF8Codec
 from zarr.codecs.pipeline import BatchedCodecPipeline
 from zarr.codecs.sharding import ShardingCodec, ShardingCodecIndexLocation
 from zarr.codecs.transpose import TransposeCodec
+from zarr.codecs.vlen_utf8 import VLenUTF8Codec
 from zarr.codecs.zstd import ZstdCodec
 
 __all__ = [
