395604dcc86b9154ea673d15d9c9bb9a74db1538
Tom Augspurger <tom.w.augspurger@gmail.com>
2024-10-09 22:46:04 -0500

    zarr.open should fall back to opening a group (#2310)
    * zarr.open should fall back to opening a group
    
    Closes https://github.com/zarr-developers/zarr-python/issues/2309
    
    * fixup
    
    * robuster
    
    * fixup test
    
    * Consistent version error
    
    * more

--- src/zarr/api/synchronous.py
+++ src/zarr/api/synchronous.py
@@ -207,6 +207,7 @@ def open_group(
     zarr_version: ZarrFormat | None = None,  # deprecated
     zarr_format: ZarrFormat | None = None,
     meta_array: Any | None = None,  # not used in async api
+    attributes: dict[str, JSON] | None = None,
 ) -> Group:
     return Group(
         sync(
@@ -221,6 +222,7 @@ def open_group(
                 zarr_version=zarr_version,
                 zarr_format=zarr_format,
                 meta_array=meta_array,
+                attributes=attributes,
             )
         )
     )
