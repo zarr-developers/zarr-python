4350e537672d036b57bcae215f1db52fb0113910
Tom Augspurger <tom.w.augspurger@gmail.com>
2024-11-08 09:07:13 -0600

    fixup

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -12,7 +12,6 @@ from numpy.testing import assert_array_equal
 
 import zarr
 from zarr import Array
-from zarr.core.array import Array
 from zarr.core.buffer import BufferPrototype, default_buffer_prototype
 from zarr.core.indexing import (
     BasicSelection,
