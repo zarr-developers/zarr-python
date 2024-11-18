87e0b83ad6c05373bc1e3174e0b93d614d45efc2
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-11-18 12:57:08 +0100

    update scratch file

--- scratch.py
+++ scratch.py
@@ -5,14 +5,14 @@ from typing import Literal
 import pytest
 
 import zarr
-from zarr.core.group import members_v3
+from zarr.core.group import iter_members, members_recursive, iter_members_deep
 from zarr.storage import MemoryStore
 from zarr.testing.store import LatencyStore
 
 
-@pytest.mark.parametrize("num_members", [4, 8, 16])
+@pytest.mark.parametrize("num_members", [10, 100, 1000])
 @pytest.mark.parametrize("method", ["default", "fast_members"])
-def test_collect_members(num_members: int, method: Literal["fast_members", "default"]) -> None:
+def test_collect_members(num_members: int, method: Literal["fast_members", "default", "fast_members_2"]) -> None:
     local_store = MemoryStore(mode="a")
     local_latency_store = LatencyStore(local_store, get_latency=0.1, set_latency=0.0)
 
@@ -21,14 +21,10 @@ def test_collect_members(num_members: int, method: Literal["fast_members", "defa
 
     for i in range(num_members):
         subgroup = root_group_raw.create_group(f"group_outer_{i}")
-        for j in range(num_members):
-            subgroup.create_group(f"group_inner_{j}")
 
     if method == "fast_members":
-
         async def amain() -> None:
-            res = await members_v3(local_latency_store, path="")
-            print(res)
+            res = [x async for x in iter_members(root_group_latency._async_group)]
 
         start = time()
         asyncio.run(amain())
