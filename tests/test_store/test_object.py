b38ada1082bf7729106175336b6b1812dee66524
Kyle Barron <kyle@developmentseed.org>
2024-11-21 14:55:01 +0000

    import or skip

--- tests/test_store/test_object.py
+++ tests/test_store/test_object.py
@@ -1,3 +1,8 @@
+# ruff: noqa: E402
+import pytest
+
+pytest.importorskip("obstore")
+
 from zarr.core.buffer import cpu
 from zarr.storage.object_store import ObjectStore
 from zarr.testing.store import StoreTests
