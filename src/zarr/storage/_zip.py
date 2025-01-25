80aea2ae5af416de15d1e4b95c50f214ee0151e4
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-01-25 01:47:30 -0700

    Improve test coverage for storage classes (#2693)
    * Run Store tests on logging
    
    * Run store tests on wrapper
    
    * Add read only open tests to WrapperStore
    
    * Ignore new coverage files
    
    * Simplify wrapper tests
    
    * Fix __eq__ method in WrapperStore
    
    * Implement __repr__ for WrapperStore
    
    * Allow separate open and init kwargs
    
    * Add open class method to LoggingStore
    
    * Add __str__ to WrapperStore
    
    * Add repr test for LoggingStore
    
    * Fix __eq__ in LoggingStore
    
    * Test getsize for stores
    
    * Test for invalid ByteRequest
    
    * Use stdout rather than stderr as the default logging stream
    
    * Test default logging stream
    
    * Add test for getsize_prefix
    
    * Document buffer prototype parameter
    
    * Add test for invalid modes in StorePath.open()
    
    * Add test for contains_group
    
    * Add tests for contains_array
    
    * Test for invalid root type for LocalStore
    
    * Test LocalStore.get with default prototype
    
    * Test for invalid set buffer arguments
    
    * Test get and set on closed stores
    
    * Test using stores in a context manager
    
    * Specify abstract methods for StoreTests
    
    * Apply suggestions from code review
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Lint
    
    * Fix typing for LoggingStore
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Match specific Errors in tests
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Add docstring
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Parametrize tests
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Test for contains group/array at multiple heirarchies
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    
    * Update TypeError on GpuMemoryStore
    
    * Don't implement _is_open setter on wrapped stores
    
    * Update reprs for LoggingStore and WrapperStore
    
    * Test check_writeable and close for WrapperStore
    
    * Update pull request template (#2717)
    
    * Add release notes
    
    * Comprehensive changelog entry
    
    * Match error message
    
    * Apply suggestions from code review
    
    Co-authored-by: David Stansby <dstansby@gmail.com>
    
    * Update 2693.bugfix.rst
    
    ---------
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>
    Co-authored-by: Hannes Spitz <44113112+brokkoli71@users.noreply.github.com>
    Co-authored-by: David Stansby <dstansby@gmail.com>
    Co-authored-by: Joe Hamman <joe@earthmover.io>

--- src/zarr/storage/_zip.py
+++ src/zarr/storage/_zip.py
@@ -149,6 +149,8 @@ class ZipStore(Store):
         prototype: BufferPrototype,
         byte_range: ByteRequest | None = None,
     ) -> Buffer | None:
+        if not self._is_open:
+            self._sync_open()
         # docstring inherited
         try:
             with self._zf.open(key) as f:  # will raise KeyError
@@ -193,6 +195,8 @@ class ZipStore(Store):
         return out
 
     def _set(self, key: str, value: Buffer) -> None:
+        if not self._is_open:
+            self._sync_open()
         # generally, this should be called inside a lock
         keyinfo = zipfile.ZipInfo(filename=key, date_time=time.localtime(time.time())[:6])
         keyinfo.compress_type = self.compression
@@ -206,9 +210,13 @@ class ZipStore(Store):
     async def set(self, key: str, value: Buffer) -> None:
         # docstring inherited
         self._check_writable()
+        if not self._is_open:
+            self._sync_open()
         assert isinstance(key, str)
         if not isinstance(value, Buffer):
-            raise TypeError("ZipStore.set(): `value` must a Buffer instance")
+            raise TypeError(
+                f"ZipStore.set(): `value` must be a Buffer instance. Got an instance of {type(value)} instead."
+            )
         with self._lock:
             self._set(key, value)
 
