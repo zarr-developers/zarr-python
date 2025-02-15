995a5d58c46c423511e62ec0cb4475176f0b3df4
brokkoli71 <brokkoli71b@gmail.com>
2025-02-15 21:27:33 +0100

    fix mypy in test_array.py

--- tests/test_array.py
+++ tests/test_array.py
@@ -1463,4 +1463,6 @@ async def test_sharding_coordinate_selection() -> None:
         shards=(2, 4, 4),
     )
     arr[:] = np.arange(2 * 3 * 4).reshape((2, 3, 4))
-    assert (arr[1, [0, 1]] == np.array([[12, 13, 14, 15], [16, 17, 18, 19]])).all()  # type: ignore[index]
+    result = arr[1, [0, 1]]
+    assert isinstance(result, NDArrayLike)
+    assert (result == np.array([[12, 13, 14, 15], [16, 17, 18, 19]])).all()  # type: ignore[index]
