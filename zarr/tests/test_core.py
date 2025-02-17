3b387af4d077f8afb04b4d083bfcaa6189803025
David Stansby <dstansby@gmail.com>
2025-02-17 11:38:33 +0000

    Deprecated partial read/writes in v2

--- zarr/tests/test_core.py
+++ zarr/tests/test_core.py
@@ -2,6 +2,8 @@ import atexit
 import sys
 import pickle
 import shutil
+import re
+
 from typing import Any, Literal, Optional, Tuple, Union, Sequence
 import unittest
 from itertools import zip_longest
@@ -84,6 +86,11 @@ from zarr.types import DIMENSION_SEPARATOR
 
 # noinspection PyMethodMayBeStatic
 
+pytestmark = [
+    pytest.mark.filterwarnings("ignore:Call to deprecated function .* \_cbuffer\_sizes.*"),
+    pytest.mark.filterwarnings("ignore:Call to deprecated function .* \_cbuffer\_metainfo.*"),
+]
+
 
 class TestArray:
     version = 2
@@ -94,7 +101,7 @@ class TestArray:
     dimension_separator: Optional[DIMENSION_SEPARATOR] = None
     cache_metadata = True
     cache_attrs = True
-    partial_decompress: bool = False
+    partial_decompress: bool | None = None
     write_empty_chunks = True
     read_only = False
     storage_transformers: Tuple[Any, ...] = ()
@@ -2481,6 +2488,9 @@ class TestArrayWithFSStoreFromFilesystem(TestArray):
 
 
 @pytest.mark.skipif(have_fsspec is False, reason="needs fsspec")
+@pytest.mark.filterwarnings(
+    "ignore:.*Support for partial decompression will be removed in a future version.*"
+)
 class TestArrayWithFSStorePartialRead(TestArray):
     compressor = Blosc(blocksize=256)
     partial_decompress = True
@@ -2547,6 +2557,9 @@ class TestArrayWithFSStoreNested(TestArrayWithFSStore):
 
 
 @pytest.mark.skipif(have_fsspec is False, reason="needs fsspec")
+@pytest.mark.filterwarnings(
+    "ignore:.*Support for partial decompression will be removed in a future version.*"
+)
 class TestArrayWithFSStoreNestedPartialRead(TestArrayWithFSStore):
     compressor = Blosc()
     dimension_separator = "/"
