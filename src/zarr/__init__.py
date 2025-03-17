5020d13c90618c434761a7f8292b2a1987d056d6
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2025-03-17 15:51:38 -0400

    feat: add  function

--- src/zarr/__init__.py
+++ src/zarr/__init__.py
@@ -36,6 +36,22 @@ from zarr.core.group import AsyncGroup, Group
 # in case setuptools scm screw up and find version to be 0.0.0
 assert not __version__.startswith("0.0.0")
 
+
+def print_debug_info() -> None:
+    import platform
+    from importlib import import_module
+
+    print(f"platform: {platform.platform()}")
+    print(f"python: {platform.python_version()}\n")
+
+    print(f"zarr: {__version__}\n")
+    for package in ["numcodecs", "numpy", "fsspec", "s3fs", "botocore", "gcsfs"]:
+        try:
+            print(f"{package}: {import_module(package).__version__}")
+        except ModuleNotFoundError:
+            continue
+
+
 __all__ = [
     "Array",
     "AsyncArray",
@@ -65,6 +81,7 @@ __all__ = [
     "open_consolidated",
     "open_group",
     "open_like",
+    "print_debug_info",
     "save",
     "save_array",
     "save_group",
