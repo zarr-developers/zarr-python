e855e54d3ac757af32f231ec26f65d9e38fcb809
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-03-17 16:27:10 +0100

    call to_dtype once in blosc evolve_from_array_spec

--- src/zarr/codecs/blosc.py
+++ src/zarr/codecs/blosc.py
@@ -136,18 +136,14 @@ class BloscCodec(BytesBytesCodec):
         }
 
     def evolve_from_array_spec(self, array_spec: ArraySpec) -> Self:
-        dtype = array_spec.dtype
+        dtype = array_spec.dtype.to_dtype()
         new_codec = self
         if new_codec.typesize is None:
-            new_codec = replace(new_codec, typesize=dtype.to_dtype().itemsize)
+            new_codec = replace(new_codec, typesize=dtype.itemsize)
         if new_codec.shuffle is None:
             new_codec = replace(
                 new_codec,
-                shuffle=(
-                    BloscShuffle.bitshuffle
-                    if dtype.to_dtype().itemsize == 1
-                    else BloscShuffle.shuffle
-                ),
+                shuffle=(BloscShuffle.bitshuffle if dtype.itemsize == 1 else BloscShuffle.shuffle),
             )
 
         return new_codec
