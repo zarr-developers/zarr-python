cdc83a8ee30391204dd3b36117401702d7211cdd
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-03-25 12:35:17 +0100

    silence mypy error about array indexing

--- tests/test_array.py
+++ tests/test_array.py
@@ -1739,4 +1739,4 @@ async def test_sharding_coordinate_selection() -> None:
         shards=(2, 4, 4),
     )
     arr[:] = np.arange(2 * 3 * 4).reshape((2, 3, 4))
-    assert (arr[1, [0, 1]] == np.array([[12, 13, 14, 15], [16, 17, 18, 19]])).all()
+    assert (arr[1, [0, 1]] == np.array([[12, 13, 14, 15], [16, 17, 18, 19]])).all()  # type: ignore[index]
