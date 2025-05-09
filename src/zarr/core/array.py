fc64cbffb008dea72d1067fe44def4c0e0d965e0
brokkoli71 <brokkoli71b@gmail.com>
2025-05-09 16:46:47 +0200

    type annotation for parse_array_metadata

--- src/zarr/core/array.py
+++ src/zarr/core/array.py
@@ -134,7 +134,7 @@ __all__ = ["create_codec_pipeline", "parse_array_metadata"]
 logger = getLogger(__name__)
 
 
-def parse_array_metadata(data: Any) -> ArrayMetadata:
+def parse_array_metadata(data: ArrayMetadata | ArrayMetadataDict) -> ArrayMetadata:
     if isinstance(data, ArrayMetadata):
         return data
     elif isinstance(data, dict):
