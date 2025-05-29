ba9a95de0318ca2f8768b86b87d1534a60a20b1f
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-05-29 21:20:09 +0200

    add vlen bytes test case

--- tests/test_regression/test_regression.py
+++ tests/test_regression/test_regression.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import numcodecs
 import numpy as np
 import pytest
-from numcodecs import LZ4, LZMA, Blosc, GZip, VLenUTF8, Zstd
+from numcodecs import LZ4, LZMA, Blosc, GZip, VLenBytes, VLenUTF8, Zstd
 
 import zarr
 from zarr.core.array import Array
@@ -41,6 +41,7 @@ class ArrayParams:
     values: np.ndarray[tuple[int], np.dtype[np.generic]]
     fill_value: np.generic | str
     compressor: numcodecs.abc.Codec
+    filters: tuple[numcodecs.abc.Codec, ...] | None = None
 
 
 basic_codecs = GZip(), Blosc(), LZ4(), LZMA(), Zstd()
@@ -68,10 +69,26 @@ vlen_string_cases = [
     ArrayParams(
         values=np.array(["a", "bb", "ccc", "dddd"], dtype="O"),
         fill_value="1",
-        compressor=VLenUTF8(),
+        compressor=None,
+        filters=(VLenUTF8(),),
     )
 ]
-array_cases = basic_array_cases + datetime_array_cases + string_array_cases + vlen_string_cases
+
+vlen_bytes_cases = [
+    ArrayParams(
+        values=np.array([b"a", b"bb", b"ccc", b"dddd"], dtype="O"),
+        fill_value=b"1",
+        compressor=None,
+        filters=(VLenBytes(),),
+    )
+]
+array_cases = (
+    basic_array_cases
+    + datetime_array_cases
+    + string_array_cases
+    + vlen_string_cases
+    + vlen_bytes_cases
+)
 
 
 @pytest.fixture
@@ -79,15 +96,14 @@ def source_array(tmp_path: Path, request: pytest.FixtureRequest) -> Array:
     dest = tmp_path / "in"
     store = LocalStore(dest)
     array_params: ArrayParams = request.param
-    compressor = array_params.compressor
     return zarr.from_array(
         store,
         data=array_params.values,
         chunks=array_params.values.shape,
-        compressors=compressor,
+        compressors=array_params.compressor,
         fill_value=array_params.fill_value,
         order="C",
-        filters=None,
+        filters=array_params.filters,
         chunk_key_encoding={"name": "v2", "configuration": {"separator": "/"}},
         write_data=True,
         zarr_format=2,
