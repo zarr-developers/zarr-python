0b7414dbbc9de84430d7ba79e78f83ab3ef726d0
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-06-18 17:04:32 -0400

    Apply suggestions from code review
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- src/zarr/abc/store.py
+++ src/zarr/abc/store.py
@@ -85,8 +85,11 @@ class Store(ABC):
 
     def with_read_only(self, read_only: bool = False) -> Store:
         """
-        Return a new store of the same type pointing to the same location with the specified read_only state.
-        The returned Store is not automatically opened.
+        Return a new store with a new read_only setting.
+        
+        The new store points to the same location with the specified new read_only state.
+        The returned Store is not automatically opened, and this store is
+        not automatically closed.
 
         Parameters
         ----------
