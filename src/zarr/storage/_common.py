a42dc7cd9e552fb82a2be8d90852057188c68b35
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-06-19 13:37:05 -0400

    Use ANY_ACCESS_MODE

--- src/zarr/storage/_common.py
+++ src/zarr/storage/_common.py
@@ -4,11 +4,18 @@ import importlib.util
 import json
 import warnings
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Literal, Self, TypeAlias, get_args
+from typing import TYPE_CHECKING, Any, Literal, Self, TypeAlias
 
 from zarr.abc.store import ByteRequest, Store
 from zarr.core.buffer import Buffer, default_buffer_prototype
-from zarr.core.common import ZARR_JSON, ZARRAY_JSON, ZGROUP_JSON, AccessModeLiteral, ZarrFormat
+from zarr.core.common import (
+    ANY_ACCESS_MODE,
+    ZARR_JSON,
+    ZARRAY_JSON,
+    ZGROUP_JSON,
+    AccessModeLiteral,
+    ZarrFormat,
+)
 from zarr.errors import ContainsArrayAndGroupError, ContainsArrayError, ContainsGroupError
 from zarr.storage._local import LocalStore
 from zarr.storage._memory import MemoryStore
@@ -97,8 +104,8 @@ class StorePath:
         if mode is None:
             return await cls._create_open_instance(store, path)
 
-        if mode not in get_args(AccessModeLiteral):
-            raise ValueError(f"Invalid mode: {mode}, expected one of {AccessModeLiteral}")
+        if mode not in ANY_ACCESS_MODE:
+            raise ValueError(f"Invalid mode: {mode}, expected one of {ANY_ACCESS_MODE}")
 
         if store.read_only:
             # Don't allow write operations on a read-only store
