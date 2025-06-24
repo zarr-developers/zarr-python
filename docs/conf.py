620749b784f51d99fc19330ae4a4c3be2b52fdda
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-06-24 14:27:52 +0200

    unexclude api from exclude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,7 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "talks", "api"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "talks"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
