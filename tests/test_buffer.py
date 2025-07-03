9da38f7565b5acf71ae13a6eee05aa2ec4e2df93
Tom Augspurger <tom.w.augspurger@gmail.com>
2025-07-03 08:51:13 -0500

    Add NDBuffer.empty (#3191)
    In https://github.com/zarr-developers/zarr-python/issues/2904, I've
    found that `np.empty(...)` can be quite a bit faster than the `np.full`
    we use in places
    (https://github.com/zarr-developers/zarr-python/blob/baabf08d07e8518e3d37bd83c493a1d46ea7ac1d/src/zarr/core/buffer/cpu.py#L149.
    Though note that `np.empty` and `np.zeros` are about the same for the
    cases where we use that.)
    
    For the common case of chunk-aligned reads, the memset used by `np.full`
    or `np.zeros` should be unnecessary, because the codec pipeline will
    overwrite the memory anyway (or overwritten with `fill_value` if the
    store is missing the key).
    
    This adds a new method `NDBuffer.empty`, mirroring `np.empty`. To
    preserve backwards compatibility, it's *not* abstract. It delegates to
    the less efficient `NDBuffer.create`. But I've implemented it for our
    `gpu` and `cpu` buffers.
    
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 import numpy as np
 import pytest
@@ -20,7 +20,7 @@ from zarr.testing.buffer import (
     TestBuffer,
     TestNDArrayLike,
 )
-from zarr.testing.utils import gpu_test
+from zarr.testing.utils import gpu_mark, gpu_test, skip_if_no_gpu
 
 if TYPE_CHECKING:
     import types
@@ -200,3 +200,39 @@ def test_gpu_buffer_prototype() -> None:
 def test_cpu_buffer_as_scalar() -> None:
     buf = cpu.buffer_prototype.nd_buffer.create(shape=(), dtype="int64")
     assert buf.as_scalar() == buf.as_ndarray_like()[()]  # type: ignore[index]
+
+
+@pytest.mark.parametrize(
+    "prototype",
+    [
+        cpu.buffer_prototype,
+        pytest.param(
+            gpu.buffer_prototype,
+            marks=[gpu_mark, skip_if_no_gpu],
+        ),
+        BufferPrototype(
+            buffer=cpu.Buffer,
+            nd_buffer=NDBufferUsingTestNDArrayLike,
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (1, 2),
+        (1, 2, 3),
+    ],
+)
+@pytest.mark.parametrize("dtype", ["int32", "float64"])
+@pytest.mark.parametrize("order", ["C", "F"])
+def test_empty(
+    prototype: BufferPrototype, shape: tuple[int, ...], dtype: str, order: Literal["C", "F"]
+) -> None:
+    buf = prototype.nd_buffer.empty(shape=shape, dtype=dtype, order=order)
+    result = buf.as_ndarray_like()
+    assert result.shape == shape
+    assert result.dtype == dtype
+    if order == "C":
+        assert result.flags.c_contiguous  # type: ignore[attr-defined]
+    else:
+        assert result.flags.f_contiguous  # type: ignore[attr-defined]
