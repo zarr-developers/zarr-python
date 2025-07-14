29d53292bdf1742635b72838eb973a7d7642eee5
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-07-14 22:09:42 +0200

    don't alias

--- src/zarr/testing/stateful.py
+++ src/zarr/testing/stateful.py
@@ -343,8 +343,8 @@ class ZarrHierarchyStateMachine(SyncMixin, RuleBasedStateMachine):
     @precondition(lambda self: len(self.all_groups) >= 2)  # fixme don't delete root
     @rule(data=st.data())
     def delete_group_using_del(self, data: DataObject) -> None:
-        members = tuple(filter(lambda v: "/" in v, sorted(self.all_groups)))
-        group_path = data.draw(st.sampled_from(members), label="Group deletion target")
+        member_names = tuple(filter(lambda v: "/" in v, sorted(self.all_groups)))
+        group_path = data.draw(st.sampled_from(member_names), label="Group deletion target")
         prefix, group_name = split_prefix_name(group_path)
         note(f"Deleting group '{group_path=!r}', {prefix=!r}, {group_name=!r} using delete")
         members = zarr.open_group(store=self.model, path=group_path).members(max_depth=None)
