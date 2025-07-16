fefb844e3285761b32f6c1a8e404bc1485f14509
Tom Augspurger <toaugspurger@nvidia.com>
2025-07-16 11:53:20 -0700

    coverage -- ignore

--- src/zarr/testing/conftest.py
+++ src/zarr/testing/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def pytest_configure(config: pytest.Config) -> None:
+def pytest_configure(config: pytest.Config) -> None:  # pragma: no cover
     # The tests in zarr.testing are intended to be run by downstream projects.
     # To allow those downstream projects to run with `--strict-markers`, we need
     # to register an entry point with pytest11 and register our "plugin" with it,
