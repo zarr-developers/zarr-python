13bcf1b14d81f8a191835d18d399b0ced1bd4321
Tom Augspurger <toaugspurger@nvidia.com>
2025-07-16 12:09:45 -0700

    Fixed coverage for real

--- src/zarr/testing/conftest.py
+++ src/zarr/testing/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def pytest_configure(config: pytest.Config) -> None:  # pragma: no cover
+def pytest_configure(config: pytest.Config) -> None:
     # The tests in zarr.testing are intended to be run by downstream projects.
     # To allow those downstream projects to run with `--strict-markers`, we need
     # to register an entry point with pytest11 and register our "plugin" with it,
