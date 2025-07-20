2cfc84809f172663a0a036eec4e2fedbc8b473e2
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-07-20 20:17:05 +0200

    expand example

--- examples/image_codecs.py
+++ examples/image_codecs.py
@@ -2,26 +2,49 @@
 # requires-python = ">=3.11"
 # dependencies = [
 #   "zarr @ file:///home/bennettd/dev/zarr-python/",
-#   "imagecodecs==2025.3.30"
+#   "imagecodecs==2025.3.30",
+#   "pytest"
 # ]
 # ///
 
 #   "zarr @ git+https://github.com/zarr-developers/zarr-python.git@main",
+from typing import Literal
 
 import numcodecs
 import numpy as np
+import pytest
 from imagecodecs.numcodecs import Jpeg
 
 import zarr
 
 numcodecs.register_codec(Jpeg)
 jpg_codec = Jpeg()
-store = {}
 
-z_w = zarr.create_array(
-    store=store, data=np.zeros((100, 100, 3), dtype=np.uint8), serializer=jpg_codec, zarr_format=3
-)
 
-z_r = zarr.open_array(store=store, zarr_format=3)
+@pytest.mark.parametrize("zarr_format", [2, 3])
+def test(zarr_format: Literal[2, 3]) -> None:
+    store = {}
+    if zarr_format == 2:
+        zarr.create_array(
+            store=store,
+            data=np.zeros((100, 100, 3), dtype=np.uint8),
+            compressors=jpg_codec,
+            zarr_format=zarr_format,
+        )
+    else:
+        zarr.create_array(
+            store=store,
+            data=np.zeros((100, 100, 3), dtype=np.uint8),
+            serializer=jpg_codec,
+            zarr_format=zarr_format,
+        )
 
-print(z_r.metadata.to_dict()["codecs"])
+    z_r = zarr.open_array(store=store, zarr_format=zarr_format)
+    if zarr_format == 2:
+        print(z_r.metadata.to_dict()["compressor"])
+    else:
+        print(z_r.metadata.to_dict()["codecs"])
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, f"-c {__file__}", "-s"])
