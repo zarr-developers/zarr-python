83d13b1a6f98688a79cd3f39333ead1b4f3ead0f
David Stansby <dstansby@gmail.com>
2025-07-28 15:57:53 +0100

    Try fixing docstrings

--- src/zarr/types.py
+++ src/zarr/types.py
@@ -5,10 +5,10 @@ from zarr.core.metadata.v2 import ArrayV2Metadata
 from zarr.core.metadata.v3 import ArrayV3Metadata
 
 AnyArray: TypeAlias = Array[Any]
-AnyArray.__doc__ = "A Zarr format 2 or 3 `Array`"
+"""A Zarr format 2 or 3 `Array`"""
 
 ArrayV2: TypeAlias = Array[ArrayV2Metadata]
-ArrayV2.__doc__ = "A Zarr format 2 `Array`"
+"""A Zarr format 2 `Array`"""
 
 ArrayV3: TypeAlias = Array[ArrayV3Metadata]
-ArrayV3.__doc__ = "A Zarr format 3 `Array`"
+"""A Zarr format 3 `Array`"""
