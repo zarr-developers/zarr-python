1d424c0ba32bb535d9f787435980e6e87bc802c4
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-07-31 23:25:36 +0200

    add tests for numcodecs compatibility

---
+++ tests/test_codecs/test_numcodecs.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from numcodecs import GZip
+
+from zarr.codecs._numcodecs import get_numcodec
+from zarr.codecs._v2 import _is_numcodec, _is_numcodec_cls
+
+
+def test_get_numcodec() -> None:
+    assert get_numcodec({"id": "gzip", "level": 2}) == GZip(level=2)
+
+
+def test_is_numcodec() -> None:
+    """
+    Test the _is_numcodec function
+    """
+    assert _is_numcodec(GZip())
+
+
+def test_is_numcodec_cls() -> None:
+    """
+    Test the _is_numcodec_cls function
+    """
+    assert _is_numcodec_cls(GZip)
