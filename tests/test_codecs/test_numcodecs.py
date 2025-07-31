c435a59728b6671250f14e4b512527bcbf329ea6
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-07-31 23:43:41 +0200

    ignore unknown key

--- tests/test_codecs/test_numcodecs.py
+++ tests/test_codecs/test_numcodecs.py
@@ -7,7 +7,7 @@ from zarr.codecs._v2 import _is_numcodec, _is_numcodec_cls
 
 
 def test_get_numcodec() -> None:
-    assert get_numcodec({"id": "gzip", "level": 2}) == GZip(level=2)
+    assert get_numcodec({"id": "gzip", "level": 2}) == GZip(level=2)  # type: ignore[typeddict-unknown-key]
 
 
 def test_is_numcodec() -> None:
