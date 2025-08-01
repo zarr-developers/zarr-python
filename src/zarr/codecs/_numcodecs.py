8e50ef8606bc0ada69127841befe179717d2a0c4
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-08-01 11:05:43 +0200

    remove re-implementation of get_codec

--- src/zarr/codecs/_numcodecs.py
+++ src/zarr/codecs/_numcodecs.py
@@ -1,5 +1,3 @@
-import numcodecs.registry as numcodecs_registry
-
 from zarr.abc.codec import CodecJSON_V2
 from zarr.codecs._v2 import Numcodec
 
@@ -22,16 +20,11 @@ def get_numcodec(data: CodecJSON_V2[str]) -> Numcodec:
     Examples
     --------
 
-    >>> codec = get_numcodec({'id': 'zlib', 'level': 1})
+    >>> codec = get_codec({'id': 'zlib', 'level': 1})
     >>> codec
     Zlib(level=1)
     """
 
-    codec_id = data["id"]
-    cls = numcodecs_registry.codec_registry.get(codec_id)
-    if cls is None and data in numcodecs_registry.entries:
-        cls = numcodecs_registry.entries[data].load()
-        numcodecs_registry.register_codec(cls, codec_id=data)
-    if cls is not None:
-        return cls.from_config({k: v for k, v in data.items() if k != "id"})  # type: ignore[no-any-return]
-    raise KeyError(data)
+    from numcodecs.registry import get_codec
+
+    return get_codec(data)  # type: ignore[no-any-return]
