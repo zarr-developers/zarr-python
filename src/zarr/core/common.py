zarr-developers/zarr-python

commit 7783dc29ace173ec86801d569d9579d0ab742b8b

chore/remove chunk coords (#3374)
* remove ChunkCoords type

* add comment

* changelog

* missing import

src/zarr/core/common.py
@@ -36,9 +36,9 @@ ZATTRS_JSON = ".zattrs"
 ZMETADATA_V2_JSON = ".zmetadata"
 
 BytesLike = bytes | bytearray | memoryview
-ShapeLike = tuple[int, ...] | int
-ChunkCoords = tuple[int, ...]
-ChunkCoordsLike = Iterable[int]
+ShapeLike = Iterable[int] | int
+# For backwards compatibility
+ChunkCoords: tuple[int, ...]
 ZarrFormat = Literal[2, 3]
 NodeType = Literal["array", "group"]
 JSON = str | int | float | Mapping[str, "JSON"] | Sequence["JSON"] | None
@@ -67,7 +67,7 @@ class NamedConfig(TypedDict, Generic[TName, TConfig]):
     """The configuration of the object."""
 
 
-def product(tup: ChunkCoords) -> int:
+def product(tup: tuple[int, ...]) -> int:
     return functools.reduce(operator.mul, tup, 1)
 
 
@@ -161,7 +161,7 @@ def parse_named_configuration(
     return name_parsed, configuration_parsed
 
 
-def parse_shapelike(data: int | Iterable[int]) -> tuple[int, ...]:
+def parse_shapelike(data: ShapeLike) -> tuple[int, ...]:
     if isinstance(data, int):
         if data < 0:
             raise ValueError(f"Expected a non-negative integer. Got {data} instead")

src/zarr/abc/codec.py
@@ -8,7 +8,7 @@ from typing_extensions import ReadOnly, TypedDict
 
 from zarr.abc.metadata import Metadata
 from zarr.core.buffer import Buffer, NDBuffer
-from zarr.core.common import ChunkCoords, NamedConfig, concurrent_map
+from zarr.core.common import NamedConfig, concurrent_map
 from zarr.core.config import config
 
 if TYPE_CHECKING:
@@ -120,7 +120,7 @@ class BaseCodec(Metadata, Generic[CodecInput, CodecOutput]):
     def validate(
         self,
         *,
-        shape: ChunkCoords,
+        shape: tuple[int, ...],
         dtype: ZDType[TBaseDType, TBaseScalar],
         chunk_grid: ChunkGrid,
     ) -> None:
@@ -129,7 +129,7 @@ class BaseCodec(Metadata, Generic[CodecInput, CodecOutput]):
 
         Parameters
         ----------
-        shape : ChunkCoords
+        shape : tuple[int, ...]
             The array shape
         dtype : np.dtype[Any]
             The array data type
@@ -335,14 +335,18 @@ class CodecPipeline:
 
     @abstractmethod
     def validate(
-        self, *, shape: ChunkCoords, dtype: ZDType[TBaseDType, TBaseScalar], chunk_grid: ChunkGrid
+        self,
+        *,
+        shape: tuple[int, ...],
+        dtype: ZDType[TBaseDType, TBaseScalar],
+        chunk_grid: ChunkGrid,
     ) -> None:
         """Validates that all codec configurations are compatible with the array metadata.
         Raises errors when a codec configuration is not compatible.
 
         Parameters
         ----------
-        shape : ChunkCoords
+        shape : tuple[int, ...]
             The array shape
         dtype : np.dtype[Any]
             The array data type

src/zarr/api/asynchronous.py
@@ -24,7 +24,6 @@ from zarr.core.buffer import NDArrayLike
 from zarr.core.common import (
     JSON,
     AccessModeLiteral,
-    ChunkCoords,
     DimensionNames,
     MemoryOrder,
     ZarrFormat,
@@ -106,7 +105,7 @@ def _infer_overwrite(mode: AccessModeLiteral) -> bool:
     return mode in _OVERWRITE_MODES
 
 
-def _get_shape_chunks(a: ArrayLike | Any) -> tuple[ChunkCoords | None, ChunkCoords | None]:
+def _get_shape_chunks(a: ArrayLike | Any) -> tuple[tuple[int, ...] | None, tuple[int, ...] | None]:
     """Helper function to get the shape and chunks from an array-like object"""
     shape = None
     chunks = None
@@ -866,9 +865,9 @@ async def open_group(
 
 
 async def create(
-    shape: ChunkCoords | int,
+    shape: tuple[int, ...] | int,
     *,  # Note: this is a change from v2
-    chunks: ChunkCoords | int | bool | None = None,
+    chunks: tuple[int, ...] | int | bool | None = None,
     dtype: ZDTypeLike | None = None,
     compressor: CompressorLike = "auto",
     fill_value: Any | None = DEFAULT_FILL_VALUE,
@@ -890,7 +889,7 @@ async def create(
     meta_array: Any | None = None,  # TODO: need type
     attributes: dict[str, JSON] | None = None,
     # v3 only
-    chunk_shape: ChunkCoords | int | None = None,
+    chunk_shape: tuple[int, ...] | int | None = None,
     chunk_key_encoding: (
         ChunkKeyEncoding
         | tuple[Literal["default"], Literal[".", "/"]]
@@ -1075,7 +1074,7 @@ async def create(
 
 
 async def empty(
-    shape: ChunkCoords, **kwargs: Any
+    shape: tuple[int, ...], **kwargs: Any
 ) -> AsyncArray[ArrayV2Metadata] | AsyncArray[ArrayV3Metadata]:
     """Create an empty array with the specified shape. The contents will be filled with the
     array's fill value or zeros if no fill value is provided.
@@ -1127,7 +1126,7 @@ async def empty_like(
 
 # TODO: add type annotations for fill_value and kwargs
 async def full(
-    shape: ChunkCoords, fill_value: Any, **kwargs: Any
+    shape: tuple[int, ...], fill_value: Any, **kwargs: Any
 ) -> AsyncArray[ArrayV2Metadata] | AsyncArray[ArrayV3Metadata]:
     """Create an array, with `fill_value` being used as the default value for
     uninitialized portions of the array.
@@ -1174,7 +1173,7 @@ async def full_like(
 
 
 async def ones(
-    shape: ChunkCoords, **kwargs: Any
+    shape: tuple[int, ...], **kwargs: Any
 ) -> AsyncArray[ArrayV2Metadata] | AsyncArray[ArrayV3Metadata]:
     """Create an array, with one being used as the default value for
     uninitialized portions of the array.
@@ -1297,7 +1296,7 @@ async def open_like(
 
 
 async def zeros(
-    shape: ChunkCoords, **kwargs: Any
+    shape: tuple[int, ...], **kwargs: Any
 ) -> AsyncArray[ArrayV2Metadata] | AsyncArray[ArrayV3Metadata]:
     """Create an array, with zero being used as the default value for
     uninitialized portions of the array.

src/zarr/api/synchronous.py
@@ -33,7 +33,6 @@ if TYPE_CHECKING:
     from zarr.core.common import (
         JSON,
         AccessModeLiteral,
-        ChunkCoords,
         DimensionNames,
         MemoryOrder,
         ShapeLike,
@@ -598,9 +597,9 @@ def create_group(
 
 # TODO: add type annotations for kwargs
 def create(
-    shape: ChunkCoords | int,
+    shape: tuple[int, ...] | int,
     *,  # Note: this is a change from v2
-    chunks: ChunkCoords | int | bool | None = None,
+    chunks: tuple[int, ...] | int | bool | None = None,
     dtype: ZDTypeLike | None = None,
     compressor: CompressorLike = "auto",
     fill_value: Any | None = DEFAULT_FILL_VALUE,  # TODO: need type
@@ -622,7 +621,7 @@ def create(
     meta_array: Any | None = None,  # TODO: need type
     attributes: dict[str, JSON] | None = None,
     # v3 only
-    chunk_shape: ChunkCoords | int | None = None,
+    chunk_shape: tuple[int, ...] | int | None = None,
     chunk_key_encoding: (
         ChunkKeyEncoding
         | tuple[Literal["default"], Literal[".", "/"]]
@@ -755,7 +754,7 @@ def create_array(
     shape: ShapeLike | None = None,
     dtype: ZDTypeLike | None = None,
     data: np.ndarray[Any, np.dtype[Any]] | None = None,
-    chunks: ChunkCoords | Literal["auto"] = "auto",
+    chunks: tuple[int, ...] | Literal["auto"] = "auto",
     shards: ShardsLike | None = None,
     filters: FiltersLike = "auto",
     compressors: CompressorsLike = "auto",
@@ -782,17 +781,17 @@ def create_array(
     name : str or None, optional
         The name of the array within the store. If ``name`` is ``None``, the array will be located
         at the root of the store.
-    shape : ChunkCoords, optional
+    shape : ShapeLike, optional
         Shape of the array. Must be ``None`` if ``data`` is provided.
     dtype : ZDTypeLike, optional
         Data type of the array. Must be ``None`` if ``data`` is provided.
     data : np.ndarray, optional
         Array-like data to use for initializing the array. If this parameter is provided, the
         ``shape`` and ``dtype`` parameters must be ``None``.
-    chunks : ChunkCoords, optional
+    chunks : tuple[int, ...], optional
         Chunk shape of the array.
         If not specified, default are guessed based on the shape and dtype.
-    shards : ChunkCoords, optional
+    shards : tuple[int, ...], optional
         Shard shape of the array. The default value of ``None`` results in no sharding at all.
     filters : Iterable[Codec], optional
         Iterable of filters to apply to each chunk of the array, in order, before serializing that
@@ -920,7 +919,7 @@ def from_array(
     data: Array | npt.ArrayLike,
     write_data: bool = True,
     name: str | None = None,
-    chunks: Literal["auto", "keep"] | ChunkCoords = "keep",
+    chunks: Literal["auto", "keep"] | tuple[int, ...] = "keep",
     shards: ShardsLike | None | Literal["keep"] = "keep",
     filters: FiltersLike | Literal["keep"] = "keep",
     compressors: CompressorsLike | Literal["keep"] = "keep",
@@ -950,22 +949,22 @@ def from_array(
     name : str or None, optional
         The name of the array within the store. If ``name`` is ``None``, the array will be located
         at the root of the store.
-    chunks : ChunkCoords or "auto" or "keep", optional
+    chunks : tuple[int, ...] or "auto" or "keep", optional
         Chunk shape of the array.
         Following values are supported:
 
         - "auto": Automatically determine the chunk shape based on the array's shape and dtype.
         - "keep": Retain the chunk shape of the data array if it is a zarr Array.
-        - ChunkCoords: A tuple of integers representing the chunk shape.
+        - tuple[int, ...]: A tuple of integers representing the chunk shape.
 
         If not specified, defaults to "keep" if data is a zarr Array, otherwise "auto".
-    shards : ChunkCoords, optional
+    shards : tuple[int, ...], optional
         Shard shape of the array.
         Following values are supported:
 
         - "auto": Automatically determine the shard shape based on the array's shape and chunk shape.
         - "keep": Retain the shard shape of the data array if it is a zarr Array.
-        - ChunkCoords: A tuple of integers representing the shard shape.
+        - tuple[int, ...]: A tuple of integers representing the shard shape.
         - None: No sharding.
 
         If not specified, defaults to "keep" if data is a zarr Array, otherwise None.
@@ -1128,7 +1127,7 @@ def from_array(
 
 
 # TODO: add type annotations for kwargs
-def empty(shape: ChunkCoords, **kwargs: Any) -> Array:
+def empty(shape: tuple[int, ...], **kwargs: Any) -> Array:
     """Create an empty array with the specified shape. The contents will be filled with the
     array's fill value or zeros if no fill value is provided.
 
@@ -1181,7 +1180,7 @@ def empty_like(a: ArrayLike, **kwargs: Any) -> Array:
 
 
 # TODO: add type annotations for kwargs and fill_value
-def full(shape: ChunkCoords, fill_value: Any, **kwargs: Any) -> Array:
+def full(shape: tuple[int, ...], fill_value: Any, **kwargs: Any) -> Array:
     """Create an array with a default fill value.
 
     Parameters
@@ -1222,7 +1221,7 @@ def full_like(a: ArrayLike, **kwargs: Any) -> Array:
 
 
 # TODO: add type annotations for kwargs
-def ones(shape: ChunkCoords, **kwargs: Any) -> Array:
+def ones(shape: tuple[int, ...], **kwargs: Any) -> Array:
     """Create an array with a fill value of one.
 
     Parameters
@@ -1324,7 +1323,7 @@ def open_like(a: ArrayLike, path: str, **kwargs: Any) -> Array:
 
 
 # TODO: add type annotations for kwargs
-def zeros(shape: ChunkCoords, **kwargs: Any) -> Array:
+def zeros(shape: tuple[int, ...], **kwargs: Any) -> Array:
     """Create an array with a fill value of zero.
 
     Parameters

src/zarr/codecs/sharding.py
@@ -36,8 +36,7 @@ from zarr.core.buffer import (
 )
 from zarr.core.chunk_grids import ChunkGrid, RegularChunkGrid
 from zarr.core.common import (
-    ChunkCoords,
-    ChunkCoordsLike,
+    ShapeLike,
     parse_enum,
     parse_named_configuration,
     parse_shapelike,
@@ -62,8 +61,8 @@ if TYPE_CHECKING:
     from zarr.core.dtype.wrapper import TBaseDType, TBaseScalar, ZDType
 
 MAX_UINT_64 = 2**64 - 1
-ShardMapping = Mapping[ChunkCoords, Buffer]
-ShardMutableMapping = MutableMapping[ChunkCoords, Buffer]
+ShardMapping = Mapping[tuple[int, ...], Buffer]
+ShardMutableMapping = MutableMapping[tuple[int, ...], Buffer]
 
 
 class ShardingCodecIndexLocation(Enum):
@@ -82,7 +81,7 @@ def parse_index_location(data: object) -> ShardingCodecIndexLocation:
 @dataclass(frozen=True)
 class _ShardingByteGetter(ByteGetter):
     shard_dict: ShardMapping
-    chunk_coords: ChunkCoords
+    chunk_coords: tuple[int, ...]
 
     async def get(
         self, prototype: BufferPrototype, byte_range: ByteRequest | None = None
@@ -114,12 +113,12 @@ class _ShardIndex(NamedTuple):
     offsets_and_lengths: npt.NDArray[np.uint64]
 
     @property
-    def chunks_per_shard(self) -> ChunkCoords:
+    def chunks_per_shard(self) -> tuple[int, ...]:
         result = tuple(self.offsets_and_lengths.shape[0:-1])
         # The cast is required until https://github.com/numpy/numpy/pull/27211 is merged
-        return cast("ChunkCoords", result)
+        return cast("tuple[int, ...]", result)
 
-    def _localize_chunk(self, chunk_coords: ChunkCoords) -> ChunkCoords:
+    def _localize_chunk(self, chunk_coords: tuple[int, ...]) -> tuple[int, ...]:
         return tuple(
             chunk_i % shard_i
             for chunk_i, shard_i in zip(chunk_coords, self.offsets_and_lengths.shape, strict=False)
@@ -131,7 +130,7 @@ class _ShardIndex(NamedTuple):
     def get_full_chunk_map(self) -> npt.NDArray[np.bool_]:
         return np.not_equal(self.offsets_and_lengths[..., 0], MAX_UINT_64)
 
-    def get_chunk_slice(self, chunk_coords: ChunkCoords) -> tuple[int, int] | None:
+    def get_chunk_slice(self, chunk_coords: tuple[int, ...]) -> tuple[int, int] | None:
         localized_chunk = self._localize_chunk(chunk_coords)
         chunk_start, chunk_len = self.offsets_and_lengths[localized_chunk]
         if (chunk_start, chunk_len) == (MAX_UINT_64, MAX_UINT_64):
@@ -139,7 +138,7 @@ class _ShardIndex(NamedTuple):
         else:
             return (int(chunk_start), int(chunk_start + chunk_len))
 
-    def set_chunk_slice(self, chunk_coords: ChunkCoords, chunk_slice: slice | None) -> None:
+    def set_chunk_slice(self, chunk_coords: tuple[int, ...], chunk_slice: slice | None) -> None:
         localized_chunk = self._localize_chunk(chunk_coords)
         if chunk_slice is None:
             self.offsets_and_lengths[localized_chunk] = (MAX_UINT_64, MAX_UINT_64)
@@ -171,7 +170,7 @@ class _ShardIndex(NamedTuple):
         )
 
     @classmethod
-    def create_empty(cls, chunks_per_shard: ChunkCoords) -> _ShardIndex:
+    def create_empty(cls, chunks_per_shard: tuple[int, ...]) -> _ShardIndex:
         offsets_and_lengths = np.zeros(chunks_per_shard + (2,), dtype="<u8", order="C")
         offsets_and_lengths.fill(MAX_UINT_64)
         return cls(offsets_and_lengths)
@@ -183,7 +182,7 @@ class _ShardReader(ShardMapping):
 
     @classmethod
     async def from_bytes(
-        cls, buf: Buffer, codec: ShardingCodec, chunks_per_shard: ChunkCoords
+        cls, buf: Buffer, codec: ShardingCodec, chunks_per_shard: tuple[int, ...]
     ) -> _ShardReader:
         shard_index_size = codec._shard_index_size(chunks_per_shard)
         obj = cls()
@@ -198,7 +197,7 @@ class _ShardReader(ShardMapping):
 
     @classmethod
     def create_empty(
-        cls, chunks_per_shard: ChunkCoords, buffer_prototype: BufferPrototype | None = None
+        cls, chunks_per_shard: tuple[int, ...], buffer_prototype: BufferPrototype | None = None
     ) -> _ShardReader:
         if buffer_prototype is None:
             buffer_prototype = default_buffer_prototype()
@@ -208,7 +207,7 @@ class _ShardReader(ShardMapping):
         obj.index = index
         return obj
 
-    def __getitem__(self, chunk_coords: ChunkCoords) -> Buffer:
+    def __getitem__(self, chunk_coords: tuple[int, ...]) -> Buffer:
         chunk_byte_slice = self.index.get_chunk_slice(chunk_coords)
         if chunk_byte_slice:
             return self.buf[chunk_byte_slice[0] : chunk_byte_slice[1]]
@@ -217,7 +216,7 @@ class _ShardReader(ShardMapping):
     def __len__(self) -> int:
         return int(self.index.offsets_and_lengths.size / 2)
 
-    def __iter__(self) -> Iterator[ChunkCoords]:
+    def __iter__(self) -> Iterator[tuple[int, ...]]:
         return c_order_iter(self.index.offsets_and_lengths.shape[:-1])
 
     def is_empty(self) -> bool:
@@ -231,8 +230,8 @@ class _ShardBuilder(_ShardReader, ShardMutableMapping):
     @classmethod
     def merge_with_morton_order(
         cls,
-        chunks_per_shard: ChunkCoords,
-        tombstones: set[ChunkCoords],
+        chunks_per_shard: tuple[int, ...],
+        tombstones: set[tuple[int, ...]],
         *shard_dicts: ShardMapping,
     ) -> _ShardBuilder:
         obj = cls.create_empty(chunks_per_shard)
@@ -248,7 +247,7 @@ class _ShardBuilder(_ShardReader, ShardMutableMapping):
 
     @classmethod
     def create_empty(
-        cls, chunks_per_shard: ChunkCoords, buffer_prototype: BufferPrototype | None = None
+        cls, chunks_per_shard: tuple[int, ...], buffer_prototype: BufferPrototype | None = None
     ) -> _ShardBuilder:
         if buffer_prototype is None:
             buffer_prototype = default_buffer_prototype()
@@ -257,13 +256,13 @@ class _ShardBuilder(_ShardReader, ShardMutableMapping):
         obj.index = _ShardIndex.create_empty(chunks_per_shard)
         return obj
 
-    def __setitem__(self, chunk_coords: ChunkCoords, value: Buffer) -> None:
+    def __setitem__(self, chunk_coords: tuple[int, ...], value: Buffer) -> None:
         chunk_start = len(self.buf)
         chunk_length = len(value)
         self.buf += value
         self.index.set_chunk_slice(chunk_coords, slice(chunk_start, chunk_start + chunk_length))
 
-    def __delitem__(self, chunk_coords: ChunkCoords) -> None:
+    def __delitem__(self, chunk_coords: tuple[int, ...]) -> None:
         raise NotImplementedError
 
     async def finalize(
@@ -286,24 +285,24 @@ class _ShardBuilder(_ShardReader, ShardMutableMapping):
 class _MergingShardBuilder(ShardMutableMapping):
     old_dict: _ShardReader
     new_dict: _ShardBuilder
-    tombstones: set[ChunkCoords] = field(default_factory=set)
+    tombstones: set[tuple[int, ...]] = field(default_factory=set)
 
-    def __getitem__(self, chunk_coords: ChunkCoords) -> Buffer:
+    def __getitem__(self, chunk_coords: tuple[int, ...]) -> Buffer:
         chunk_bytes_maybe = self.new_dict.get(chunk_coords)
         if chunk_bytes_maybe is not None:
             return chunk_bytes_maybe
         return self.old_dict[chunk_coords]
 
-    def __setitem__(self, chunk_coords: ChunkCoords, value: Buffer) -> None:
+    def __setitem__(self, chunk_coords: tuple[int, ...], value: Buffer) -> None:
         self.new_dict[chunk_coords] = value
 
-    def __delitem__(self, chunk_coords: ChunkCoords) -> None:
+    def __delitem__(self, chunk_coords: tuple[int, ...]) -> None:
         self.tombstones.add(chunk_coords)
 
     def __len__(self) -> int:
         return self.old_dict.__len__()
 
-    def __iter__(self) -> Iterator[ChunkCoords]:
+    def __iter__(self) -> Iterator[tuple[int, ...]]:
         return self.old_dict.__iter__()
 
     def is_empty(self) -> bool:
@@ -335,7 +334,7 @@ class ShardingCodec(
 ):
     """Sharding codec"""
 
-    chunk_shape: ChunkCoords
+    chunk_shape: tuple[int, ...]
     codecs: tuple[Codec, ...]
     index_codecs: tuple[Codec, ...]
     index_location: ShardingCodecIndexLocation = ShardingCodecIndexLocation.end
@@ -343,7 +342,7 @@ class ShardingCodec(
     def __init__(
         self,
         *,
-        chunk_shape: ChunkCoordsLike,
+        chunk_shape: ShapeLike,
         codecs: Iterable[Codec | dict[str, JSON]] = (BytesCodec(),),
         index_codecs: Iterable[Codec | dict[str, JSON]] = (BytesCodec(), Crc32cCodec()),
         index_location: ShardingCodecIndexLocation | str = ShardingCodecIndexLocation.end,
@@ -413,7 +412,7 @@ class ShardingCodec(
     def validate(
         self,
         *,
-        shape: ChunkCoords,
+        shape: tuple[int, ...],
         dtype: ZDType[TBaseDType, TBaseScalar],
         chunk_grid: ChunkGrid,
     ) -> None:
@@ -646,14 +645,14 @@ class ShardingCodec(
             )
 
     def _is_total_shard(
-        self, all_chunk_coords: set[ChunkCoords], chunks_per_shard: ChunkCoords
+        self, all_chunk_coords: set[tuple[int, ...]], chunks_per_shard: tuple[int, ...]
     ) -> bool:
         return len(all_chunk_coords) == product(chunks_per_shard) and all(
             chunk_coords in all_chunk_coords for chunk_coords in c_order_iter(chunks_per_shard)
         )
 
     async def _decode_shard_index(
-        self, index_bytes: Buffer, chunks_per_shard: ChunkCoords
+        self, index_bytes: Buffer, chunks_per_shard: tuple[int, ...]
     ) -> _ShardIndex:
         index_array = next(
             iter(
@@ -686,7 +685,7 @@ class ShardingCodec(
         assert isinstance(index_bytes, Buffer)
         return index_bytes
 
-    def _shard_index_size(self, chunks_per_shard: ChunkCoords) -> int:
+    def _shard_index_size(self, chunks_per_shard: tuple[int, ...]) -> int:
         return (
             get_pipeline_class()
             .from_codecs(self.index_codecs)
@@ -695,7 +694,7 @@ class ShardingCodec(
             )
         )
 
-    def _get_index_chunk_spec(self, chunks_per_shard: ChunkCoords) -> ArraySpec:
+    def _get_index_chunk_spec(self, chunks_per_shard: tuple[int, ...]) -> ArraySpec:
         return ArraySpec(
             shape=chunks_per_shard + (2,),
             dtype=UInt64(endianness="little"),
@@ -715,7 +714,7 @@ class ShardingCodec(
             prototype=shard_spec.prototype,
         )
 
-    def _get_chunks_per_shard(self, shard_spec: ArraySpec) -> ChunkCoords:
+    def _get_chunks_per_shard(self, shard_spec: ArraySpec) -> tuple[int, ...]:
         return tuple(
             s // c
             for s, c in zip(
@@ -726,7 +725,7 @@ class ShardingCodec(
         )
 
     async def _load_shard_index_maybe(
-        self, byte_getter: ByteGetter, chunks_per_shard: ChunkCoords
+        self, byte_getter: ByteGetter, chunks_per_shard: tuple[int, ...]
     ) -> _ShardIndex | None:
         shard_index_size = self._shard_index_size(chunks_per_shard)
         if self.index_location == ShardingCodecIndexLocation.start:
@@ -743,14 +742,14 @@ class ShardingCodec(
         return None
 
     async def _load_shard_index(
-        self, byte_getter: ByteGetter, chunks_per_shard: ChunkCoords
+        self, byte_getter: ByteGetter, chunks_per_shard: tuple[int, ...]
     ) -> _ShardIndex:
         return (
             await self._load_shard_index_maybe(byte_getter, chunks_per_shard)
         ) or _ShardIndex.create_empty(chunks_per_shard)
 
     async def _load_full_shard_maybe(
-        self, byte_getter: ByteGetter, prototype: BufferPrototype, chunks_per_shard: ChunkCoords
+        self, byte_getter: ByteGetter, prototype: BufferPrototype, chunks_per_shard: tuple[int, ...]
     ) -> _ShardReader | None:
         shard_bytes = await byte_getter.get(prototype=prototype)
 

src/zarr/codecs/transpose.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from zarr.abc.codec import ArrayArrayCodec
 from zarr.core.array_spec import ArraySpec
-from zarr.core.common import JSON, ChunkCoordsLike, parse_named_configuration
+from zarr.core.common import JSON, parse_named_configuration
 from zarr.registry import register_codec
 
 if TYPE_CHECKING:
@@ -35,7 +35,7 @@ class TransposeCodec(ArrayArrayCodec):
 
     order: tuple[int, ...]
 
-    def __init__(self, *, order: ChunkCoordsLike) -> None:
+    def __init__(self, *, order: Iterable[int]) -> None:
         order_parsed = parse_transpose_order(order)
 
         object.__setattr__(self, "order", order_parsed)

commit bebbac06bdef61c90ab81a7e6f6e5b68db63be5a

fix: `ChunkCoords` definition
Broken since #3374

src/zarr/core/common.py
@@ -38,7 +38,7 @@ ZMETADATA_V2_JSON = ".zmetadata"
 BytesLike = bytes | bytearray | memoryview
 ShapeLike = Iterable[int] | int
 # For backwards compatibility
-ChunkCoords: tuple[int, ...]
+ChunkCoords = tuple[int, ...]
 ZarrFormat = Literal[2, 3]
 NodeType = Literal["array", "group"]
 JSON = str | int | float | Mapping[str, "JSON"] | Sequence["JSON"] | None