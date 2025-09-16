11e44a0fea0ea000a019532f131912df37d7d83d
Davis Bennett <davis.v.bennett@gmail.com>
2025-09-16 22:21:56 +0200

    Update src/zarr/core/array.py
    Co-authored-by: Tom Augspurger <tom.augspurger88@gmail.com>

--- src/zarr/core/array.py
+++ src/zarr/core/array.py
@@ -4252,8 +4252,8 @@ async def from_array(
         which is to choose default filters based on the data type of the array and the Zarr format specified.
         For all data types in Zarr V3, and most data types in Zarr V2, the default filters are the empty tuple ``()``.
         The only cases where default filters are not empty is when the Zarr format is 2, and the
-        data type is a variable-length data type like `:class:zarr.dtype.VariableLengthUTF8` or
-        `:class:zarr.dtype.VariableLengthUTF8`. In these cases, the default filters is a tuple with a
+        data type is a variable-length data type like :class:`zarr.dtype.VariableLengthUTF8` or
+        :class:`zarr.dtype.VariableLengthUTF8`. In these cases, the default filters is a tuple with a
         single element which is a codec specific to that particular data type.
 
         To create an array with no filters, provide an empty iterable or the value ``None``.
