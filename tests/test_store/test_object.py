ffde28d11aad5e97bdf123a23d666016b8aa0093
David Stansby <dstansby@gmail.com>
2025-10-02 15:16:57 +0100

    Adapt ObjectStore tests for new generic class

--- tests/test_store/test_object.py
+++ tests/test_store/test_object.py
@@ -22,8 +22,8 @@ class StoreKwargs(TypedDict):
     read_only: bool
 
 
-class TestObjectStore(StoreTests[ObjectStore, cpu.Buffer]):
-    store_cls = ObjectStore
+class TestObjectStore(StoreTests[ObjectStore[LocalStore], cpu.Buffer]):
+    store_cls = ObjectStore[LocalStore]
     buffer_cls = cpu.Buffer
 
     @pytest.fixture
@@ -32,35 +32,35 @@ class TestObjectStore(StoreTests[ObjectStore, cpu.Buffer]):
         return {"store": store, "read_only": False}
 
     @pytest.fixture
-    def store(self, store_kwargs: StoreKwargs) -> ObjectStore:
+    def store(self, store_kwargs: StoreKwargs) -> ObjectStore[LocalStore]:
         return self.store_cls(**store_kwargs)
 
-    async def get(self, store: ObjectStore, key: str) -> Buffer:
+    async def get(self, store: ObjectStore[LocalStore], key: str) -> Buffer:
         assert isinstance(store.store, LocalStore)
         new_local_store = LocalStore(prefix=store.store.prefix)
         return self.buffer_cls.from_bytes(obstore.get(new_local_store, key).bytes())
 
-    async def set(self, store: ObjectStore, key: str, value: Buffer) -> None:
+    async def set(self, store: ObjectStore[LocalStore], key: str, value: Buffer) -> None:
         assert isinstance(store.store, LocalStore)
         new_local_store = LocalStore(prefix=store.store.prefix)
         obstore.put(new_local_store, key, value.to_bytes())
 
-    def test_store_repr(self, store: ObjectStore) -> None:
+    def test_store_repr(self, store: ObjectStore[LocalStore]) -> None:
         from fnmatch import fnmatch
 
         pattern = "ObjectStore(object_store://LocalStore(*))"
         assert fnmatch(f"{store!r}", pattern)
 
-    def test_store_supports_writes(self, store: ObjectStore) -> None:
+    def test_store_supports_writes(self, store: ObjectStore[LocalStore]) -> None:
         assert store.supports_writes
 
-    def test_store_supports_partial_writes(self, store: ObjectStore) -> None:
+    def test_store_supports_partial_writes(self, store: ObjectStore[LocalStore]) -> None:
         assert not store.supports_partial_writes
 
-    def test_store_supports_listing(self, store: ObjectStore) -> None:
+    def test_store_supports_listing(self, store: ObjectStore[LocalStore]) -> None:
         assert store.supports_listing
 
-    def test_store_equal(self, store: ObjectStore) -> None:
+    def test_store_equal(self, store: ObjectStore[LocalStore]) -> None:
         """Test store equality"""
         # Test equality against a different instance type
         assert store != 0
@@ -78,15 +78,15 @@ class TestObjectStore(StoreTests[ObjectStore, cpu.Buffer]):
     def test_store_init_raises(self) -> None:
         """Test __init__ raises appropriate error for improper store type"""
         with pytest.raises(TypeError):
-            ObjectStore("path/to/store")  # type: ignore[arg-type]
+            ObjectStore("path/to/store")  # type: ignore[type-var]
 
-    async def test_store_getsize(self, store: ObjectStore) -> None:
+    async def test_store_getsize(self, store: ObjectStore[LocalStore]) -> None:
         buf = cpu.Buffer.from_bytes(b"\x01\x02\x03\x04")
         await self.set(store, "key", buf)
         size = await store.getsize("key")
         assert size == len(buf)
 
-    async def test_store_getsize_prefix(self, store: ObjectStore) -> None:
+    async def test_store_getsize_prefix(self, store: ObjectStore[LocalStore]) -> None:
         buf = cpu.Buffer.from_bytes(b"\x01\x02\x03\x04")
         await self.set(store, "c/key1/0", buf)
         await self.set(store, "c/key2/0", buf)
