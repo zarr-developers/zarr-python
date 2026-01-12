171ad85bc05ae232d8d2333af68edca556bd5be3
Mathieu Scheltienne <mathieu.scheltienne@dandelion.science>
2026-01-12 17:11:49 +0100

    fix docstring

--- src/zarr/_compat.py
+++ src/zarr/_compat.py
@@ -99,7 +99,9 @@ def _reshape_view(arr: "NDArray[Any]", shape: tuple[int, ...]) -> "NDArray[Any]"
     Raises
     ------
     AttributeError
-        If a view cannot be created (the array is not contiguous).
+        If a view cannot be created (the array is not contiguous) on NumPy < 2.1.
+    ValueError
+        If a view cannot be created (the array is not contiguous) on NumPy >= 2.1.
     """
     if Version(np.__version__) >= Version("2.1"):
         return arr.reshape(shape, copy=False)
