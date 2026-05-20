b3861c89629e2d0fb5cfa3d8573183c049f4004f
ilan-gold <ilanbassgold@gmail.com>
2026-05-20 16:05:39 +0200

    don't shard unless there is something to shard

--- src/zarr/testing/strategies.py
+++ src/zarr/testing/strategies.py
@@ -305,7 +305,7 @@ def arrays(
         else:
             chunks_param = chunk_grid_meta.chunk_shape
 
-            if all(s > c and c > 0 for s, c in zip(nparray.shape, chunks_param, strict=True)):
+            if all(s > c and c > 1 for s, c in zip(nparray.shape, chunks_param, strict=True)):
                 shard_shape = draw(
                     st.none() | shard_shapes(shape=nparray.shape, chunk_shape=chunks_param),
                     label="shard shape",
@@ -350,6 +350,7 @@ def arrays(
             assert a.metadata.chunk_grid.chunk_shape == (
                 a.shards if shard_shape is not None else a.chunks
             )
+            assert shard_shape == a.shards
         else:
             assert isinstance(a.metadata.chunk_grid, RectilinearChunkGridMetadata)
             assert shard_shape is None
